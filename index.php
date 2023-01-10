6d6a3ce6b7fb29e28c2748d1df5df2454243f8a5
Patrick Minogue <patrick.minogue@gmail.com>
2023-01-10 07:59:53 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Acnespecialisten</title>
+    <link rel="stylesheet"
+          href="normalize.css">
+    <link rel="stylesheet"
+          href="style.css">
+</head>
+
+<body>
+    <h1>Hello</h1>
+</body>
+
+</html>
