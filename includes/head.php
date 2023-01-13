e9aaa23858c287362d8ac144ae92290ffdcb996d
Patrick Minogue <patrick.minogue@gmail.com>
2023-01-13 21:09:21 +0000

    removed commented out code

--- includes/head.php
+++ includes/head.php
@@ -7,22 +7,6 @@ include('util/functions.php');
 <meta name="author"
       content="Acnespecialisten Sverige AB">
 <base href="/">
-<!-- <link rel="preconnect"
-      href="https://fonts.googleapis.com">
-<link rel="preconnect"
-      href="https://fonts.gstatic.com"
-      crossorigin> -->
-<!-- Google fonts deferred -->
-<!-- <link rel="preload"
-      href="https://fonts.googleapis.com/css2?family=DM+Serif+Text&family=Inter:wght@400;600&display=swap"
-      as="style"
-      onload="this.onload=null;this.rel='stylesheet'">
-<noscript>
-      <link rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=DM+Serif+Text&family=Inter:wght@400;600&display=swap">
-</noscript> -->
-<!-- End google fonts deferred -->
-
 <link rel="stylesheet"
       href="styles/normalize.css">
 <link rel="stylesheet"
