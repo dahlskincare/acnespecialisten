14ae4e79cf8cedf9dab6473933e71da4af71cd5e
Patrick Minogue <patrick.minogue@gmail.com>
2023-01-13 13:23:28 +0000

    created banner

--- index.php
+++ index.php
@@ -22,7 +22,78 @@
 <body>
       <?php include('includes/header.php'); ?>
       <main>
-            <!-- TODO: Page Content Here -->
+            <section>
+                  <div id="banner">
+                        <div class="container">
+                              <h1 class="big">Skin specialists</h1>
+                        </div>
+                  </div>
+                  <div class="container">
+                        <div class="banner-bar"
+                             id="banner-bar-small">
+                              <a href="https://dahlskincare.com/skin-consultation"
+                                 target="_blank">
+                                    <button class="white">
+                                          <span>Get a free consultation</span>
+                                          <?php icon('link-external') ?>
+                                    </button>
+                              </a>
+                              <div id="banner-bar-cols">
+                                    <div>
+                                          <span class="h500">4,8</span>
+                                          <?php icon('trustpilot') ?>
+                                          <div class="p200">on Trustpilot</div>
+                                    </div>
+                                    <div>
+                                          <span class="h500">30+</span>
+                                          <div class="p200">years</div>
+                                    </div>
+                                    <div>
+                                          <span class="h500">200k+</span>
+                                          <div class="p200">clients</div>
+                                    </div>
+                              </div>
+                        </div>
+                        <div class="banner-bar"
+                             id="banner-bar-large">
+                              <div id="banner-bar-cols">
+                                    <div class="flex-aligner">
+                                          <span class="h500">4,8</span>
+                                          <div>
+                                                <?php icon('trustpilot') ?>
+                                                <?php icon('trustpilot') ?>
+                                                <?php icon('trustpilot') ?>
+                                                <?php icon('trustpilot') ?>
+                                                <?php icon('trustpilot') ?>
+                                                <div class="p200">Trustpilot score</div>
+                                          </div>
+                                    </div>
+                                    <div class="flex-aligner">
+                                          <span class="h500">30+</span>
+                                          <div>
+                                                <div class="h400">years</div>
+                                                <div class="p200">on the market</div>
+                                          </div>
+                                    </div>
+                                    <div class="flex-aligner">
+                                          <span class="h500">200k+</span>
+                                          <div>
+                                                <div class="h400">clients</div>
+                                                <div class="p200">were satisfied</div>
+                                          </div>
+                                    </div>
+                                    <a href="https://dahlskincare.com/skin-consultation"
+                                       target="_blank">
+                                          <button class="white">
+                                                <span>Get a free consultation</span>
+                                                <?php icon('link-external') ?>
+                                          </button>
+                                    </a>
+                              </div>
+                        </div>
+                  </div>
+                  </div>
+            </section>
       </main>
       <?php include('includes/footer.php'); ?>
 </body>
