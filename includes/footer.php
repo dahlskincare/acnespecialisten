883536aa24bb5e7dd870084c41825ac9dc234ea8
Patrick Minogue <patrick.minogue@gmail.com>
2023-01-13 14:30:40 +0000

    implemented small footer salon cards

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,113 @@
 <footer>
-    <hr>
-    <p>FOOTER</p>
+    <div class="container">
+        <!-- Mobile footer -->
+        <div id="footer-small">
+            <div id="brands">
+                <a href="https://www.klarna.com"
+                   target="_blank">
+                    <img src="images/brands/brand-icon-klarna.png"
+                         alt="klarna"
+                         width="40"
+                         height="40" />
+                </a>
+                <a href="https://www.amex.com"
+                   target="_blank">
+                    <img src="images/brands/brand-icon-amex.png"
+                         alt="amex"
+                         width="40"
+                         height="40" />
+                </a>
+                <a href="https://www.qliro.com/sv-se"
+                   target="_blank">
+                    <img src="images/brands/brand-icon-qliro.png"
+                         alt="qliro"
+                         width="40"
+                         height="40" />
+                </a>
+                <a href="https://www.swish.nu">
+                    <img src="images/brands/brand-icon-swish.png"
+                         alt="swish"
+                         width="40"
+                         height="40" />
+                </a>
+                <a href="https://www.if.se">
+                    <img src="images/brands/brand-icon-if.png"
+                         alt="if"
+                         width="40"
+                         height="40" />
+                </a>
+                <a href="https://www.shr.nu">
+                    <img src="images/brands/brand-icon-shr.png"
+                         alt="shr"
+                         width="40"
+                         height="40" />
+                </a>
+                <a href="https://ww.bokadirekt.se">
+                    <img src="images/brands/brand-icon-bokadirekt.png"
+                         alt="bokadirekt"
+                         width="40"
+                         height="40" />
+                </a>
+            </div>
+            <div id="contact">
+                <h2>Acnespecialisten</h2>
+                <div id="phoneDotEmail"
+                     class="p200">
+                    <span>+46 (0) 8-29 61 01</span>
+                    <div class="dot"></div>
+                    <span>kund@acnespecialisten.se</span>
+                </div>
+                <div id="buttons">
+                    <button class="grey">Call us</button>
+                    <button class="grey">Email us</button>
+                </div>
+            </div>
+            <div id="salon-cards">
+                <div class="salon-card">
+                    <div class="flex-aligner">
+                        <img src="images/salons/profile-sundbyberg.jpg"
+                             alt="Sundbyberg"
+                             width="102"
+                             height="102">
+                        <div>
+                            <h2>Sundbyberg</h2>
+                            <p class="p200">Sturegatan 32, 172 31 Sundbyberg</p>
+                        </div>
+                    </div>
+                    <button class="grey">Book a visit</button>
+                </div>
+                <div class="salon-card">
+                    <div class="flex-aligner">
+                        <img src="images/salons/profile-ostermalm.jpg"
+                             alt="Östermalm"
+                             width="102"
+                             height="102">
+                        <div>
+                            <h2>Östermalm</h2>
+                            <p class="p200">Strandvägen 7, 114 51 Stockholm</p>
+                        </div>
+                    </div>
+                    <button class="grey">Book a visit</button>
+                </div>
+                <div class="salon-card">
+                    <div class="flex-aligner">
+                        <img src="images/salons/profile-sodermalm.jpg"
+                             alt="Södermalm"
+                             width="102"
+                             height="102">
+                        <div>
+                            <h2>Södermalm</h2>
+                            <p class="p200">Hornsgatan 47, 118 49 Stockholm</p>
+                        </div>
+                    </div>
+                    <button class="grey">Book a visit</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Desktop footer -->
+        <div id="footer-large">
+            LARGE FOOTER HERE
+        </div>
+    </div>
 </footer>
