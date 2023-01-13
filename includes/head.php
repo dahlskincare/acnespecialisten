985513b8a256e8b888d3de7c0358bb5ed6c58662
Patrick Minogue <patrick.minogue@gmail.com>
2023-01-13 14:52:29 +0000

    display swap instead of block

--- includes/head.php
+++ includes/head.php
@@ -12,7 +12,7 @@ include('util/functions.php');
 <link rel="preconnect"
       href="https://fonts.gstatic.com"
       crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text&family=Inter:wght@400;600&display=block"
+<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text&family=Inter:wght@400;600&display=swap"
       rel="stylesheet">
 <link rel="stylesheet"
       href="styles/normalize.css">
