803a836272aee71b787a04eff9917195f3178f98
Patrick Minogue <patrick.minogue@gmail.com>
2023-01-13 12:24:03 +0000

    set default paddings for mobile

--- includes/header.php
+++ includes/header.php
@@ -1,45 +1,41 @@
 <header>
     <div class="container">
-        <div class="columns">
-            <div class="column">
-                <div id="header-small">
-                    <?php icon("search") ?>
-                    <a href="/">
-                        <img id="logo"
-                             src="images/logo-green.svg"
-                             alt="Acnespecialisten"
-                             width="139"
-                             height="25" />
-                    </a>
-                    <?php icon("bars") ?>
-                </div>
-                <div id="header-large">
-                    <div id="left">
-                        <a href="/">
-                            <img id="logo"
-                                 src="images/logo-green.svg"
-                                 alt="Acnespecialisten"
-                                 width="160"
-                                 height="28" />
-                        </a>
-                        <nav>
-                            <ul class="b100">
-                                <li>Problems</li>
-                                <li>Services</li>
-                                <li>Brands</li>
-                                <li>Prices</li>
-                                <li>Contact</li>
-                            </ul>
-                        </nav>
-                    </div>
-                    <div id="actions">
-                        <a class="button text"
-                           href="https://dahlskincare.com/skin-consultation"
-                           target="_blank">Get a free consultation</a>
-                        <?php icon("search") ?>
-                        <?php icon("bars") ?>
-                    </div>
-                </div>
+        <div id="header-small">
+            <?php icon("search") ?>
+            <a href="/">
+                <img id="logo"
+                     src="images/logo-green.svg"
+                     alt="Acnespecialisten"
+                     width="139"
+                     height="25" />
+            </a>
+            <?php icon("bars") ?>
+        </div>
+        <div id="header-large">
+            <div id="left">
+                <a href="/">
+                    <img id="logo"
+                         src="images/logo-green.svg"
+                         alt="Acnespecialisten"
+                         width="160"
+                         height="28" />
+                </a>
+                <nav>
+                    <ul class="b100">
+                        <li>Problems</li>
+                        <li>Services</li>
+                        <li>Brands</li>
+                        <li>Prices</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </div>
+            <div id="actions">
+                <a class="button text"
+                   href="https://dahlskincare.com/skin-consultation"
+                   target="_blank">Get a free consultation</a>
+                <?php icon("search") ?>
+                <?php icon("bars") ?>
             </div>
         </div>
     </div>
