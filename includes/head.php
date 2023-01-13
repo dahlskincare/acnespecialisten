cf7f98da52d6dcdab899352182bc1f4e94b578ae
Patrick Minogue <patrick.minogue@gmail.com>
2023-01-13 15:11:58 +0000

    set maximum scale to 5.0

--- includes/head.php
+++ includes/head.php
@@ -2,7 +2,7 @@
 include('util/functions.php');
 ?>
 <meta name="viewport"
-      content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+      content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
 <meta charset="UTF-8">
 <meta name="author"
       content="Acnespecialisten Sverige AB">
