864ec6e41b4d8c4b99d9b3c5abe5275512be9cf6
Patrick Minogue <patrick.minogue@gmail.com>
2023-01-13 12:24:12 +0000

    preload banner images

--- includes/head.php
+++ includes/head.php
@@ -26,3 +26,11 @@ include('util/functions.php');
 <link rel="preload"
       as="image"
       href="images/logo-green.svg">
+<link rel="preload"
+      as="image"
+      href="images/banner-desktop.jpg"
+      media="(min-width: 984px)">
+<link rel="preload"
+      as="image"
+      href="images/banner-mobile.jpg"
+      media="(max-width: 983px)">
