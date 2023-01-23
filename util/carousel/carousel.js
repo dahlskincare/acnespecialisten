c3886be348b68c0b0236039d127482584e42494a
Patrick Minogue <patrick.minogue@gmail.com>
2023-01-23 11:17:46 +0000

    include margin and border in carousel scroll

--- util/carousel/carousel.js
+++ util/carousel/carousel.js
@@ -2,11 +2,11 @@ Carousel = {
     prev: (carouselId) => {
         const carousel = document.querySelector(carouselId);
         let child = carousel.children[0];
-        carousel.scrollLeft -= child.clientWidth;
+        carousel.scrollLeft -= child.getBoundingClientRect().width;
     },
     next: (carouselId) => {
         const carousel = document.querySelector(carouselId);
         let child = carousel.children[0];
-        carousel.scrollLeft += child.clientWidth;
+        carousel.scrollLeft += child.getBoundingClientRect().width;
     }
 }
