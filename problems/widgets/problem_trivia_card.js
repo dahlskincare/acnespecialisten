e9753e294ef3544fb074f55e7c56c8cae2aff857
Patrick Minogue <patrick.minogue@gmail.com>
2023-02-01 11:27:00 +0000

    implemented problem trivia card large screen

---
+++ problems/widgets/problem_trivia_card.js
@@ -0,0 +1,5 @@
+function showExtendedContent(button) {
+    button.classList.add('is-hidden');
+    button.parentElement.querySelector("#extended-content").classList.remove('is-hidden');
+}
+//# sourceMappingURL=problem_trivia_card.js.map
--- problems/widgets/problem_trivia_card.php
+++ problems/widgets/problem_trivia_card.php
@@ -1,17 +1,29 @@
 <div class="problem-trivia-card-widget">
     <h3 class="h300">
-        <span class="prefix">
+        <span class="trivia-prefix">
             <?php echo $trivia->prefix ?>
         </span>
         <span>
             <?php echo $trivia->title ?>
         </span>
     </h3>
+    <?php if (sizeof($trivia->icons) > 0) { ?>
+        <div class="trivia-icons mt-m">
+            <?php foreach ($trivia->icons as $icon => $label) { ?>
+                <div class="trivia-icon">
+                    <?php
+                    icon($icon);
+                    echo '<span>' . $label . '</span>';
+                    ?>
+                </div>
+            <?php } ?>
+        </div>
+    <?php } ?>
     <p class="p200 mt-m">
         <?php echo $trivia->content ?>
     </p>
     <button class="mt-m text b200 compact"
-            onclick="showExtendedContent()">
+            onclick="showExtendedContent(this)">
         <span class="l10n">Read more</span>
         <?php icon('arrow-down') ?>
     </button>
