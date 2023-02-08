7ce4c60b2a4a171a2525ab0e48acdb810d6b02e8
Patrick Minogue <patrick.minogue@gmail.com>
2023-02-08 12:52:37 +0000

    added header for acne types

--- problems/types/index.php
+++ problems/types/index.php
@@ -1,4 +1,21 @@
 <?php include_once($_SERVER['DOCUMENT_ROOT'] . '/config.php'); ?>
+
+<?php
+$image_small = 'https://via.placeholder.com/358x274.jpg';
+$image_large = 'https://via.placeholder.com/424x456.jpg';
+
+if ($lang == 'sv') {
+    $title = 'Olika typer av akneärr';
+    $description = 'Here we explain what identifies acne scars, why the problem occurs and how we can help you treat. Here we explain what identifies acne scars, why the problem occurs and how we can help you treat.';
+    $booking_url = '';
+} else {
+    $title = 'Different type of acne scars';
+    $description = 'Here we explain what identifies acne scars, why the problem occurs and how we can help you treat. Here we explain what identifies acne scars, why the problem occurs and how we can help you treat.';
+    $booking_url = '';
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="<?php echo $lang ?>">
 
@@ -11,24 +28,98 @@
 
     <!-- Optional: Set canonical version of this page (https://support.google.com/webmasters/answer/10347851) -->
 
-    <!-- Default imports (metadata, fonts, google analytics etc) -->
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
-
-    <!-- This stylesheet should be included only on pages with the default style and layout. -->
     <link rel="stylesheet" href="/styles/default-layout.css">
-
-    <!-- 
-            TODO: Create a css file with custom styles specific to the new page. 
-            The css file should be have the same name and be put in the same folder 
-            as the php file (my-folder/my-new-page.php -> my-new-page.css, etc).
-      -->
-    <link rel="stylesheet" href="/[FOLDER NAME HERE]/[FILENAME HERE].css">
+    <link rel="stylesheet" href="problems/types/index.css">
 </head>
 
 <body>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'); ?>
+    <div class="is-hidden-touch is-hidden-desktop-only" id="floater">
+        <div class="container">
+            <div id="floating-picture" style="background-image: url('<?php echo $image_large ?>')">
+                <div id="overlay">
+                    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/widgets/badges/badges.php'); ?>
+                    <div>
+                        <h2 class="h600">
+                            <?php echo $title ?>
+                        </h2>
+                        <p class="mt-m">
+                            hello
+                        <div class="mt-xl columns is-2 is-variable">
+                            <div class="column">
+                                <a href="https://www.dahlskincare.com/skin-consultation" target="_blank" class="button white expand l10n">Get a free consultation</a>
+                            </div>
+                            <div class="column">
+                                <a href="<?php echo $booking_url ?>" target="_blank" class="button white expand l10n">Book a treatment</a>
+                            </div>
+                        </div>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <main>
-        <!-- TODO: Page Content Here -->
+        <section id="header">
+            <div id="green-header-small" class="is-hidden-tablet">
+                <div class="container">
+                    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/widgets/breadcrumbs/breadcrumbs.php'); ?>
+                    <h1 class="mt-xs h600">
+                        <?php echo $title ?>
+                    </h1>
+                    <p class="mt-m">
+                        <?php echo $description ?>
+                    </p>
+                    <div class="mt-xl columns is-mobile">
+                        <div class="column is-half">
+                            <a href="https://dahlskincare.com/skin-consultation" target="_blank" class="button b200 white expand l10n">Free consultation</a>
+                        </div>
+                        <div class="column is-half">
+                            <a href="<?php echo $booking_url ?>" target="_blank" class="button b200 white expand l10n">Book a treatment</a>
+                        </div>
+                    </div>
+                    <div class="mt-xl">
+                        <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/widgets/badges/badges.php'); ?>
+                    </div>
+                </div>
+            </div>
+            <div id="green-header-large" class="is-hidden-mobile">
+                <div class="container">
+                    <div class="columns">
+                        <div class="column is-half">
+                            <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/widgets/breadcrumbs/breadcrumbs.php'); ?>
+                        </div>
+                        <div class="column is-half flex-row align-end justify-end">
+                            <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/widgets/badges/badges.php'); ?>
+                        </div>
+                    </div>
+                    <div id="green-header-large-text" class="mt-xxs">
+                        <h1 class="h600">
+                            <?php echo $title ?>
+                        </h1>
+                        <p class="mt-s p200">
+                            <?php echo $description ?>
+                        </p>
+                        <div class="mt-xl flex-row" id="book-buttons">
+                            <a href="https://www.dahlskincare.com/skin-consultation" target="_blank" class="button b200 white l10n">Get a free consultation</a>
+                            <a href="<?php echo $booking_url ?>" target="_blank" class="button b200 white l10n">Book a treatment</a>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="container">
+            <div id="content">
+                <section id="image" class="mt-xl is-hidden-desktop">
+                    <picture class="is-hidden-tablet">
+                        <source media="(max-width: 449px)" srcset="<?php echo $image_small ?>">
+                        <source media="(min-width: 450px)" srcset="<?php echo $image_large ?>">
+                        <img src="<?php echo $image_large ?>" alt="<?php echo $title ?>" width="358" height="274" />
+                    </picture>
+                </section>
     </main>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
 </body>
