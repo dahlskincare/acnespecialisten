d3deea3a577218d36ed650cd5d3a825b4f593ef5
Pontus Larsson <pontus.larsson.torner@gmail.com>
2023-02-08 22:17:34 +0000

    working on brands

--- includes/widgets/brands/brands.php
+++ includes/widgets/brands/brands.php
@@ -1,42 +1,42 @@
 <div class="brands-widget">
-  <div class="columns">
+  <div class="flex-container">
 
-    <div class="column">
+    <div class="flex-item-desktop flex-item-top-left">
       <img id="img"
            src="images/brands/powerlite.svg"
            alt="">
     </div>
-    <div class="column">
+    <div class="flex-item-desktop flex-item-top-right">
       <img id="img"
            src="images/brands/dermapen4.svg"
            alt="">
     </div>
-    <div class="column">
+    <div class="flex-item-desktop">
       <img id="img"
            src="images/brands/cryopen.svg"
            alt="">
     </div>
-    <div class="column">
+    <div class="flex-item-desktop">
       <img id="img"
            src="images/brands/lumenis.svg"
            alt="">
     </div>
-    <div class="column">
+    <div class="flex-item-desktop">
       <img id="img"
            src="images/brands/alma.svg"
            alt="">
     </div>
-    <div class="column">
+    <div class="flex-item-desktop">
       <img id="img"
            src="images/brands/prxt.svg"
            alt="">
     </div>
-    <div class="column">
+    <div class="flex-item-desktop flex-item-bottom-left">
       <img id="img"
            src="images/brands/hydrafacial.svg"
            alt="">
     </div>
-    <div class="column">
+    <div class="flex-item-desktop flex-item-bottom-right">
       <img id="img"
            src="images/brands/ipinfusion.svg"
            alt="">
@@ -44,9 +44,10 @@
 
   </div>
 
+<div>
 
 
 
 
 
-</div>
+
