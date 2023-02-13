4d23ab934ca35672855d28603d5c7f329bfcba1c
Patrick Minogue <patrick.minogue@gmail.com>
2023-02-13 10:36:14 +0000

    added skeleton page for type details

---
+++ problems/types/type/type.php
@@ -0,0 +1,31 @@
+<?php include_once($_SERVER['DOCUMENT_ROOT'] . '/config.php'); ?>
+<!DOCTYPE html>
+<html lang="<?php echo $lang ?>">
+
+<head>
+    <!-- TODO: Set title and meta tags -->
+    <title class="l10n">Acnespecialisten</title>
+    <meta name="description" content="" class="l10n">
+    <meta name="title" content="" class="l10n">
+    <meta name="keywords" content="" class="l10n">
+
+    <!-- Optional: Set canonical version of this page (https://support.google.com/webmasters/answer/10347851) -->
+
+    <!-- Default imports (metadata, fonts, google analytics etc) -->
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
+
+    <!-- This stylesheet should be included only on pages with the default style and layout. -->
+    <link rel="stylesheet" href="/styles/default-layout.css">
+
+    <link rel="stylesheet" href="problems/types/type/type.css">
+</head>
+
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'); ?>
+    <main>
+        <!-- TODO: Page Content Here -->
+    </main>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
+</body>
+
+</html>
