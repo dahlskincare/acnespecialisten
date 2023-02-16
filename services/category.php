2ab4244f6429e5e6beb2be370bbd7e6f54455406
Patrick Minogue <patrick.minogue@gmail.com>
2023-02-16 10:30:51 +0000

    added skeleton for service category and service

---
+++ services/category.php
@@ -0,0 +1,28 @@
+<?php include_once($_SERVER['DOCUMENT_ROOT'] . '/config.php'); ?>
+<!DOCTYPE html>
+<html lang="<?php echo $lang ?>">
+
+<head>
+    <!-- TODO: Set title and meta tags -->
+    <title class="l10n">Acnespecialisten</title>
+    <meta name="description" content="" class="l10n">
+    <meta name="title" content="" class="l10n">
+    <meta name="keywords" content="" class="l10n">
+
+    <!-- Optional: Set canonical version of this page (https://support.google.com/webmasters/answer/10347851) -->
+
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
+
+    <link rel="stylesheet" href="/styles/default-layout.css">
+    <link rel="stylesheet" href="/services/category.css">
+</head>
+
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'); ?>
+    <main>
+        <h1>hello category <?php echo $_GET['id'] ?></h1>
+    </main>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
+</body>
+
+</html>
