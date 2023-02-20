c1d4c7a35dda8f1e8b6db4a80b7542ff937cb35e
Patrick Minogue <patrick.minogue@gmail.com>
2023-02-20 07:52:19 +0000

    added some style to logo

--- includes/header.php
+++ includes/header.php
@@ -1,29 +1,46 @@
 <header>
     <div class="container">
-        <div class="is-hidden-desktop" id="header-small">
+        <div class="is-hidden-desktop"
+             id="header-small">
             <?php icon("search") ?>
             <a href="/">
-                <img id="logo" src="images/logo-green.svg" alt="Acnespecialisten" width="139" height="25" />
+                <img id="logo"
+                     src="images/logo-green.svg"
+                     alt="Acnespecialisten"
+                     width="139"
+                     height="25" />
             </a>
             <?php icon("bars") ?>
         </div>
-        <div class="is-hidden-touch" id="header-large">
+        <div class="is-hidden-touch"
+             id="header-large">
             <div id="left">
-                <a href="/">
-                    <img id="logo" src="images/logo-green.svg" alt="Acnespecialisten" width="160" height="28" />
+                <a href="/"
+                   id="logo">
+                    <img src="images/logo-green.svg"
+                         alt="Acnespecialisten"
+                         width="160"
+                         height="28" />
                 </a>
                 <nav>
                     <ul>
-                        <li><a href="problems" class="h100 l10n">Problems</a></li>
-                        <li><a href="services" class="h100 l10n">Services</a></li>
-                        <li><a href="brands" class="h100 l10n">Brands</a></li>
-                        <li><a href="prices" class="h100 l10n">Prices</a></li>
-                        <li><a href="contact" class="h100 l10n">Contact</a></li>
+                        <li><a href="problems"
+                               class="h100 l10n">Problems</a></li>
+                        <li><a href="services"
+                               class="h100 l10n">Services</a></li>
+                        <li><a href="brands"
+                               class="h100 l10n">Brands</a></li>
+                        <li><a href="prices"
+                               class="h100 l10n">Prices</a></li>
+                        <li><a href="contact"
+                               class="h100 l10n">Contact</a></li>
                     </ul>
                 </nav>
             </div>
             <div id="actions">
-                <a class="button text l10n" href="https://dahlskincare.com/skin-consultation" target="_blank">Get a free consultation</a>
+                <a class="button text l10n"
+                   href="https://dahlskincare.com/skin-consultation"
+                   target="_blank">Get a free consultation</a>
                 <?php icon("search") ?>
                 <?php icon("bars") ?>
             </div>
