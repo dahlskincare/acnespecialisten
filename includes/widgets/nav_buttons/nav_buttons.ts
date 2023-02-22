2fec009f51ddcd5cb376c61d9690cc8837dc2d93
Patrick Minogue <patrick.minogue@gmail.com>
2023-02-22 12:22:10 +0000

    only show scroll buttons on desktop

--- includes/widgets/nav_buttons/nav_buttons.ts
+++ includes/widgets/nav_buttons/nav_buttons.ts
@@ -1,33 +1,36 @@
-let _scrollStep = 200;
-let _widget = document.querySelector('.nav-buttons-widget');
-let _carousel = _widget.querySelector('#nav-buttons-carousel');
-let _buttonPrevContainer = _widget.querySelector('#scroll-prev');
-let _buttonNextContainer = _widget.querySelector('#scroll-next');
-let _buttonPrev = _buttonPrevContainer.querySelector('button');
-let _buttonNext = _buttonNextContainer.querySelector('button');
-if (_carousel.scrollWidth > _widget.parentElement.clientWidth) {
-    _buttonPrev.addEventListener('click', () => {
-        _carousel.scrollBy(-_scrollStep, 0);
-    });
-    _buttonNext.addEventListener('click', () => {
-        _carousel.scrollBy(_scrollStep, 0);
-    });
-    _buttonNextContainer.classList.remove('is-hidden');
+if (document.body.clientWidth > 800) {
 
-    _carousel.addEventListener('scroll', () => {
-        let showNext = _carousel.scrollWidth - _carousel.scrollLeft != _carousel.clientWidth;
-        let showPrevious = _carousel.scrollLeft > 0;
+    let _scrollStep = 200;
+    let _widget = document.querySelector('.nav-buttons-widget');
+    let _carousel = _widget.querySelector('#nav-buttons-carousel');
+    let _buttonPrevContainer = _widget.querySelector('#scroll-prev');
+    let _buttonNextContainer = _widget.querySelector('#scroll-next');
+    let _buttonPrev = _buttonPrevContainer.querySelector('button');
+    let _buttonNext = _buttonNextContainer.querySelector('button');
+    if (_carousel.scrollWidth > _widget.parentElement.clientWidth) {
+        _buttonPrev.addEventListener('click', () => {
+            _carousel.scrollBy(-_scrollStep, 0);
+        });
+        _buttonNext.addEventListener('click', () => {
+            _carousel.scrollBy(_scrollStep, 0);
+        });
+        _buttonNextContainer.classList.remove('is-hidden');
 
-        if (showNext) {
-            _buttonNextContainer.classList.remove('is-hidden');
-        } else {
-            _buttonNextContainer.classList.add('is-hidden');
-        }
+        _carousel.addEventListener('scroll', () => {
+            let showNext = _carousel.scrollWidth - _carousel.scrollLeft != _carousel.clientWidth;
+            let showPrevious = _carousel.scrollLeft > 0;
 
-        if (showPrevious) {
-            _buttonPrevContainer.classList.remove('is-hidden');
-        } else {
-            _buttonPrevContainer.classList.add('is-hidden');
-        }
-    });
+            if (showNext) {
+                _buttonNextContainer.classList.remove('is-hidden');
+            } else {
+                _buttonNextContainer.classList.add('is-hidden');
+            }
+
+            if (showPrevious) {
+                _buttonPrevContainer.classList.remove('is-hidden');
+            } else {
+                _buttonPrevContainer.classList.add('is-hidden');
+            }
+        });
+    }
 }
