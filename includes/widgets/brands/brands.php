739122bbfa7bb25aacfb5b1d410924194449d3d5
Patrick Minogue <patrick.minogue@gmail.com>
2023-02-22 14:02:38 +0000

    set width and height attributes

--- includes/widgets/brands/brands.php
+++ includes/widgets/brands/brands.php
@@ -1,31 +1,28 @@
 <div class="brands-widget">
   <div class="flex-container">
-
     <div class="flex-item-top-left flex-item">
-      <img src="images/brands/powerlite.svg" alt="">
+      <img src="images/brands/powerlite.svg" alt="powerlite" width="116" height="30">
     </div>
     <div class="flex-item flex-item-top-right flex-item-img-top-right">
-      <img src="images/brands/dermapen4.svg" alt="">
+      <img src="images/brands/dermapen4.svg" alt="dermapen4" width="116" height="30">
     </div>
     <div class="flex-item flex-item-other">
-      <img src="images/brands/cryopen.svg" alt="">
+      <img src="images/brands/cryopen.svg" alt="cryopen" width="116" height="30">
     </div>
     <div class="flex-item flex-item-other">
-      <img src="images/brands/lumenis.svg" alt="">
+      <img src="images/brands/lumenis.svg" alt="lumenis" width="116" height="30">
     </div>
     <div class="flex-item flex-item-other">
-      <img src="images/brands/alma.svg" alt="">
+      <img src="images/brands/alma.svg" alt="alma" width="116" height="30">
     </div>
     <div class="flex-item flex-item-other">
-      <img src="images/brands/prxt.svg" alt="">
+      <img src="images/brands/prxt.svg" alt="prxt" width="116" height="30">
     </div>
     <div class="flex-item flex-item-bottom-left">
-      <img src="images/brands/hydrafacial.svg" alt="">
+      <img src="images/brands/hydrafacial.svg" alt="hydrafacial" width="116" height="30">
     </div>
     <div class="flex-item flex-item-bottom-right">
-      <img src="images/brands/ipinfusion.svg" alt="">
+      <img src="images/brands/ipinfusion.svg" alt="ipinfusion" width="116" height="30">
     </div>
-
   </div>
-
   <div>
