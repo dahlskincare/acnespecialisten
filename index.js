1e1a243acea55b80ee61bb4dff9bfee14d4e91c6
Patrick Minogue <patrick.minogue@gmail.com>
2023-02-23 13:06:10 +0000

    added video on small

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+var video = document.querySelector('#banner-video');
 if (document.body.clientWidth > 800) {
-    var video = document.querySelector('#banner-video');
     video.src = 'videos/banner.mp4';
+}
+else {
+    video.src = 'videos/banner-square.mp4';
 }
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 
+let video = document.querySelector('#banner-video') as HTMLVideoElement;
 if (document.body.clientWidth > 800) {
-    let video = document.querySelector('#banner-video') as HTMLVideoElement;
     video.src = 'videos/banner.mp4';
+} else {
+    video.src = 'videos/banner-square.mp4';
 }
