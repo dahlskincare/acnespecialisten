f6691d1b15b4f970b1315fcadac1599a3b6992a4
Patrick Minogue <patrick.minogue@gmail.com>
2023-02-23 11:07:20 +0000

    user placeholder images in problem article

--- problems/widgets/problem-article/problem-article.php
+++ problems/widgets/problem-article/problem-article.php
@@ -1,14 +1,17 @@
 <?php
 class ProblemArticle
 {
-    public function __construct($id, $title, $content)
+    public function __construct($id, $title, $image_small, $image_large, $content)
     {
         $this->id = $id;
         $this->title = $title;
+        $this->image_small = $image_small;
+        $this->image_large = $image_large;
         $this->content = $content;
     }
     public $id;
     public $title;
+    public $image_small;
+    public $image_large;
     public $content;
 }
-?>
