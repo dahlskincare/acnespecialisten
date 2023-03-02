e9fb83c450f8d71e9712d9dd45d8d45d7fe4b94e
Patrick Minogue <patrick.minogue@gmail.com>
2023-03-02 14:27:28 +0000

    removed mock service desc

--- services/service.php
+++ services/service.php
@@ -7,7 +7,7 @@ $model = new Service(
     title: 'Problem skin facials',
     duration: '50 min',
     price: '995 kr',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    content: '',
     image_small: 'https://via.placeholder.com/358x274.webp',
     image_large: 'https://via.placeholder.com/424x456.webp',
     consultation_url: 'https://bokadirekt.se',
