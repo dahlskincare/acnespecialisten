c8a76af45666c0093f06e22105556e0ed01d6694
Patrick Minogue <patrick.minogue@gmail.com>
2023-03-02 10:34:54 +0000

    implemented combos large

--- includes/widgets/service_combo/service_combo.php
+++ includes/widgets/service_combo/service_combo.php
@@ -2,10 +2,10 @@
     <div class="service-combo-small is-hidden-tablet">
         <div class="image">
             <picture>
-                <!-- We use the large image on small screens -->
+                <!-- We actually use the large image on small screens -->
                 <source media="(max-width: 799px)" srcset="<?php echo $combo->image_large ?>">
                 <source media="(min-width: 800px)" srcset="<?php echo $combo->image_small ?>">
-                <img src="images/about-mobile.jpg" alt="About us" width="361" height="274" />
+                <img src="<?php echo $combo->image_large ?>" alt="Combo image" width="361" height="274" />
             </picture>
         </div>
         <div class="service-combo-content">
@@ -25,6 +25,30 @@
         </div>
     </div>
     <div class="service-combo-large is-hidden-mobile">
-        LARGE
+        <div class="image">
+            <picture>
+                <img src="<?php echo $combo->image_small ?>" alt="Combo image" width="200" height="200" />
+            </picture>
+        </div>
+        <div class="service-combo-content">
+            <div class="service-combo-items">
+                <?php foreach ($combo->items as $combo_item) { ?>
+                    <div class="service-combo-item">
+                        <span class="h200"><?php echo $combo_item->title ?></span>
+                        <span class="p200"><?php echo $combo_item->price ?></span>
+                    </div>
+                <?php } ?>
+            </div>
+            <div class="service-combo-info">
+                <div class="service-combo-duration">
+                    <span class="l10n">Duration:</span>
+                    <span><?php echo $combo->duration ?></span>
+                </div>
+                <div>
+                    <span class="service-combo-full-price"><?php echo $combo->full_price ?></span><span class="service-combo-price"><?php echo $combo->price ?></span>
+                    <a href="<?php $combo->booking_url ?>" target="_blank" class="button l10n">Book a combination</a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
