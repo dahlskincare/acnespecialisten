9c3debf6d307e04ecba7934d8b2c66c82cd2a9e6
Patrick Minogue <patrick.minogue@gmail.com>
2023-03-06 11:42:07 +0000

    implemented header for customer results page

--- results/index.php
+++ results/index.php
@@ -1,10 +1,16 @@
-<?php include_once($_SERVER['DOCUMENT_ROOT'] . '/config.php'); ?>
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
+
+
+$consultation_url = '';
+
+?>
 <!DOCTYPE html>
 <html lang="<?php echo $lang ?>">
 
 <head>
     <!-- TODO: Set title and meta tags -->
-    <title class="l10n">Acnespecialisten</title>
+    <title class="l10n">Acnespecialisten | Customer results</title>
     <meta name="description" content="" class="l10n">
     <meta name="title" content="" class="l10n">
     <meta name="keywords" content="" class="l10n">
@@ -16,7 +22,67 @@
 <body>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'); ?>
     <main>
-        <!-- TODO: Page Content Here -->
+        <section id="banner" class="sticky-badges-target">
+            <div id="banner-green">
+                <div class="container l10n">
+                    <div class="is-hidden-desktop">
+                        <h1 class="h600 mt-xs l10n">Customer results</h1>
+                        <p class="p200 mt-xs l10n">
+                            In a personal meeting with a skin specialist, your skin type is examined and identified. We take pre-photos of your skin, recommend. In a personal meeting with a skin specialist, your skin type is examined and identified. We take pre-photos of your skin, recommend. In a personal meeting with a skin specialist, your skin type is examined and identified In a personal meeting with a skin specialist, your skinonal...
+                            In a personal meeting with a skin specialist, your skin type is examined and identified. We take pre-photos of your skin, recommend. In a personal meeting with a skin specialist, your skin type is examined and identified. We take pre-photos of your skin, recommend. In a personal meeting with a skin specialist, your skin type is examined and identified In a personal meeting with a skin specialist, your skinonal...
+                        </p>
+                        <a href="<?php echo $consultation_url ?>" target="_blank" class="button b200 white expand mt-xl l10n">Get a free consultation</a>
+                    </div>
+                    <div class="is-hidden-touch" id="banner-green-desktop">
+                        <div class="flex-row justify-space-between">
+                            <div></div>
+                            <div class="mt-xl mb-xs">
+                                <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/widgets/badges/badges.php'); ?>
+                            </div>
+                        </div>
+                        <div class="flex-row align-end">
+                            <div id="skin-problems-header-column">
+                                <h1 class="h600 mt-xs l10n">Customer<br />results</h1>
+                                <a href="<?php echo $consultation_url ?>" class="button b200 white mt-xl">Get a free consultation</a>
+                            </div>
+                            <div class="l10n">
+                                In a personal meeting with a skin specialist, your skin type is examined and identified. We take pre-photos of your skin, recommend. In a personal meeting with a skin specialist, your skin type is examined and identified. We take pre-photos of your skin, recommend. In a personal meeting with a skin specialist, your skin type is examined and identified In a personal meeting with a skin specialist, your skinonal...
+                            </div>
+                            <div class="ml-xl4 l10n">
+                                In a personal meeting with a skin specialist, your skin type is examined and identified. We take pre-photos of your skin, recommend. In a personal meeting with a skin specialist, your skin type is examined and identified. We take pre-photos of your skin, recommend. In a personal meeting with a skin specialist, your skin type is examined and identified In a personal meeting with a skin specialist, your skinonal...
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-m is-hidden-desktop">
+                <div class="container">
+                    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/widgets/badges/badges.php'); ?>
+                </div>
+                <div class="container" id="filters-touch-container">
+                    <div id="filters-touch" class="mt-m">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div id="filters-desktop" class="is-hidden-touch">
+                    <div id="filter-items">
+
+                    </div>
+                    <div class="filter-button is-hidden" id="filter-button-previous">
+                        <button class="round-large grey">
+                            <?php icon('arrow-left') ?>
+                        </button>
+                    </div>
+                    <div class="filter-button is-hidden" id="filter-button-next">
+                        <button class="round-large grey">
+                            <?php icon('arrow-right') ?>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
 </body>
