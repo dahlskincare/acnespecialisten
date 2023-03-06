6575e8cc03cbc0755063d268284e10eabefe5262
Patrick Minogue <patrick.minogue@gmail.com>
2023-03-06 10:35:01 +0000

    implemented skeleton results

---
+++ results/index.php
@@ -0,0 +1,24 @@
+<?php include_once($_SERVER['DOCUMENT_ROOT'] . '/config.php'); ?>
+<!DOCTYPE html>
+<html lang="<?php echo $lang ?>">
+
+<head>
+    <!-- TODO: Set title and meta tags -->
+    <title class="l10n">Acnespecialisten</title>
+    <meta name="description" content="" class="l10n">
+    <meta name="title" content="" class="l10n">
+    <meta name="keywords" content="" class="l10n">
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
+    <link rel="stylesheet" href="/styles/default-layout.css">
+    <link rel="stylesheet" href="/results/style.css">
+</head>
+
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'); ?>
+    <main>
+        <!-- TODO: Page Content Here -->
+    </main>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
+</body>
+
+</html>
