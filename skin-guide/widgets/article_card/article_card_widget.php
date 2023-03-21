374f4abbdb30d0081bc714ca779987d63386b8f3
Patrick Minogue <patrick.minogue@gmail.com>
2023-03-21 10:51:46 +0000

    made whole article card clickable

--- skin-guide/widgets/article_card/article_card_widget.php
+++ skin-guide/widgets/article_card/article_card_widget.php
@@ -1,10 +1,10 @@
-<div class="article-card-widget">
+<a class="article-card-widget" href="skin-guide/articles/<?php echo $article->id ?>">
     <img src="<?php echo $article->image_small ?>" alt="<?php echo $article->title ?>" width="426" height="324" />
     <div class="ac-content">
         <div class="ac-problem"><?php echo $article->problem ?></div>
         <div class="ac-title"><?php echo $article->title ?></div>
         <div class="ac-description"><?php echo $article->description ?></div>
-        <a href="skin-guide/articles/<?php echo $article->id ?>" class="ac-read-more is-hidden-desktop button outline expand l10n">Read more</a>
-        <a href="skin-guide/articles/<?php echo $article->id ?>" class="ac-read-more is-hidden-touch l10n">Read more</a>
+        <button class="ac-read-more is-hidden-desktop button outline expand l10n">Read more</button>
+        <div class="ac-read-more is-hidden-touch l10n">Read more</div>
     </div>
-</div>
+</a>
