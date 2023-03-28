1e1d72bda3f5e5e2810063ffb8b06afea1c9fe92
Patrick Minogue <patrick.minogue@gmail.com>
2023-03-28 16:20:36 +0000

    added overlay image

--- brands/widgets/brand_card/brand_card.php
+++ brands/widgets/brand_card/brand_card.php
@@ -1,27 +1,27 @@
 <div class="brand-card-widget">
-
-  <img class="imic" src="https://via.placeholder.com/424x324.webp" alt="">
-
+  <img class="imic" src="https://via.placeholder.com/424x324.webp" alt="" width="424" height="324">
   <div class="containerr">
-  <div class="laser">Laser for problem skin</div>
-  <div class="alma">Alma - Hybrid (CO2)</div>
-  <div>
-  <div class="descript">In a personal meeting with a skin specialist, your skin type is examined and identified. We take pre-photos of your skin, recommend. In a personal In a personal meeting with a skin specialist, your skin type is examined and identified. We take pre-photos of your skin, recommend.  In a personal personal meeting with a skin specialist, your skin...</div>
-  <div class="readmore" >
-  <a href="url">Read more</a>
-  </div>
-  </div>
-  
-  <hr class="hrr">
-  
-  <div class="flex-container">
+    <div class="flex-row justify-end">
+      <img src="https://via.placeholder.com/116x80.webp" class="overlay" alt="overlay" width="116" height="80">
+    </div>
+    <div class="laser">Laser for problem skin</div>
+    <div class="alma">Alma - Hybrid (CO2)</div>
+    <div>
+      <div class="descript">In a personal meeting with a skin specialist, your skin type is examined and identified. We take pre-photos of your skin, recommend. In a personal In a personal meeting with a skin specialist, your skin type is examined and identified. We take pre-photos of your skin, recommend. In a personal personal meeting with a skin specialist, your skin...</div>
+      <div class="readmore">
+        <a href="url">Read more</a>
+      </div>
+    </div>
 
-   <button class="left">Get a free consultation</button>
-  
-  <button class="right">Book a treatment</button>
-  </div>
- 
+    <hr class="hrr">
+
+    <div class="flex-container">
+
+      <button class="left">Get a free consultation</button>
+
+      <button class="right">Book a treatment</button>
+    </div>
+
   </div>
 
 </div>
-
