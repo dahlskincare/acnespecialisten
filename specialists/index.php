500b9c01ab2fb0e0622174486056ef8e685a9e6d
Patrick Minogue <patrick.minogue@gmail.com>
2023-04-17 11:37:51 +0000

    added specialists title

---
+++ specialists/index.php
@@ -0,0 +1,41 @@
+<?php include_once($_SERVER['DOCUMENT_ROOT'] . '/config.php'); ?>
+<!DOCTYPE html>
+<html lang="<?php echo $lang ?>">
+
+<head>
+    <!-- TODO: Set title and meta tags -->
+    <title class="l10n">Acnespecialisten | Specialists</title>
+    <meta name="description" content="" class="l10n">
+    <meta name="title" content="" class="l10n">
+    <meta name="keywords" content="" class="l10n">
+
+    <!-- Optional: Set canonical version of this page (https://support.google.com/webmasters/answer/10347851) -->
+
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
+    <link rel="stylesheet" href="/styles/default-layout.css">
+    <link rel="stylesheet" href="/specialists/specialists.css">
+</head>
+
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'); ?>
+    <main>
+        <section id="banner">
+            <div class="container">
+                <div class="flex-row justify-space-between">
+                    <div class="is-hidden-touch">
+                    </div>
+                    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/widgets/badges/badges.php'); ?>
+                </div>
+            </div>
+        </section>
+        <div class="container">
+            <section id="title" class="large-margin">
+                <h1 class="l10n">Our specialists</h1>
+                <div class="p200 mt-s">At Acnespecialisten there is an experienced and well-educated group of doctors with specialist skills and broad experience from the aesthetic and reconstructive industry.</div>
+            </section>
+        </div>
+    </main>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
+</body>
+
+</html>
