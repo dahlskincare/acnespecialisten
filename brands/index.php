3645fc2c5edb2f489edba9dc354fb02093dd6c88
Patrick Minogue <patrick.minogue@gmail.com>
2023-05-03 15:11:12 +0000

    brand card mobile implemented

--- brands/index.php
+++ brands/index.php
@@ -5,29 +5,33 @@
 <head>
       <!-- TODO: Set title and meta tags -->
       <title class="l10n">Acnespecialisten | Brands</title>
-      <meta name="description"
-            content=""
-            class="l10n">
-      <meta name="title"
-            content=""
-            class="l10n">
-      <meta name="keywords"
-            content=""
-            class="l10n">
-
+      <meta name="description" content="" class="l10n">
+      <meta name="title" content="" class="l10n">
+      <meta name="keywords" content="" class="l10n">
       <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
-
-      <link rel="stylesheet"
-            href="/styles/default-layout.css">
-
-      <link rel="stylesheet"
-            href="/brands/style.css">
+      <link rel="stylesheet" href="/styles/default-layout.css">
+      <link rel="stylesheet" href="/brands/style.css">
 </head>
 
 <body>
       <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'); ?>
       <main>
-            <?php include('widgets/brand_card/brand_card.php') ?>
+            <div class="container">
+                  <?php
+                  $brand = new Brand(
+                        label: 'Alma',
+                        image: 'https://via.placeholder.com/116x80.webp',
+                        photo: 'https://via.placeholder.com/424x324.webp',
+                        title: 'Alma - Hybrid (CO2)',
+                        subtitle: 'Laser for problem skin',
+                        description: 'In a personal meeting with a skin specialist, your skin type is examined and identified. We take pre-photos of your skin, recommend. In a personal In a personal meeting with a skin specialist, your skin type is examined and identified. We take pre-photos of your skin, recommend. In a personal personal meeting with a skin specialist, your skin...',
+                        url: 'url',
+                        booking_url: 'treatment_url',
+                        consultation_url: 'consultation_url'
+                  );
+                  include('widgets/brand_card/brand_card.php');
+                  ?>
+            </div>
       </main>
       <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
 </body>
