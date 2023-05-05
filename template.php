49091b740c46b768f445a6269d73e9acb9d4247e
Patrick Minogue <patrick.minogue@gmail.com>
2023-05-05 09:55:06 +0000

    added opengraph placholders to template

--- template.php
+++ template.php
@@ -5,15 +5,15 @@
 <head>
       <!-- TODO: Set title and meta tags -->
       <title class="l10n">Acnespecialisten</title>
-      <meta name="description"
-            content=""
-            class="l10n">
-      <meta name="title"
-            content=""
-            class="l10n">
-      <meta name="keywords"
-            content=""
-            class="l10n">
+      <meta name="description" content="" class="l10n">
+      <meta name="title" content="" class="l10n">
+      <meta name="keywords" content="" class="l10n">
+      <meta property="og:title" content="Acnespecialisten" />
+      <meta property="og:description" content="" class="l10n" />
+      <meta property="og:image" content="images/about-desktop.jpg" />
+      <meta property="twitter:title" content="Acnespecialisten" />
+      <meta property="twitter:description" content="" class="l10n" />
+      <meta property="twitter:image" content="images/about-desktop.jpg" />
 
       <!-- Optional: Set canonical version of this page (https://support.google.com/webmasters/answer/10347851) -->
 
@@ -21,16 +21,14 @@
       <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
 
       <!-- This stylesheet should be included only on pages with the default style and layout. -->
-      <link rel="stylesheet"
-            href="/styles/default-layout.css">
+      <link rel="stylesheet" href="/styles/default-layout.css">
 
       <!-- 
             TODO: Create a css file with custom styles specific to the new page. 
             The css file should be have the same name and be put in the same folder 
             as the php file (my-folder/my-new-page.php -> my-new-page.css, etc).
       -->
-      <link rel="stylesheet"
-            href="/[FOLDER NAME HERE]/[FILENAME HERE].css">
+      <link rel="stylesheet" href="/[FOLDER NAME HERE]/[FILENAME HERE].css">
 </head>
 
 <body>
