e6f6cf5ff0d290add6de250ae9a6a95c7767980c
Mac <68950292+macentoshMD@users.noreply.github.com>
2023-05-07 16:37:59 +0200

    Update index.php

--- problem/index.php
+++ problem/index.php
@@ -469,7 +469,7 @@ $problem_areas = array(
                               </div>
                         </div>
                         <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/widgets/reviews/reviews.php'); ?>
-                        <a class="mt-xl button outline expand is-hidden-tablet l10n" href="omdomen">View all reviews</a>
+                        <a class="mt-xl button outline expand is-hidden-tablet l10n" href="omdomen">Läs fler omdömen</a>
                   </div>
             </section>
             <section id="faq">
