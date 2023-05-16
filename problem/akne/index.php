86baa93b68d7c96755e91437cb5e4eca9709c005
Patrick Minogue <patrick.minogue@gmail.com>
2023-05-16 20:19:23 +0000

    fixed array

--- problem/akne/index.php
+++ problem/akne/index.php
@@ -527,11 +527,12 @@ $faq_categories = array(
                         <p class="p200 mt-m">Undvik att peta eller klämma på behandlade områden för att undvika eventuell ärrbildning och infektion. Dessutom är det bra att skydda huden från solen, då den kan vara extra känslig efter behandling. Använd solskyddsmedel och undvik direkt solljus så mycket som möjligt. Kom ihåg att vi på Acnespecialisten alltid finns här för dig om du har några frågor eller funderingar efter din behandling.</p>'
             ),
             new Question(
-                  title:'Eftervård efter behandlingen?',
+                  title: 'Eftervård efter behandlingen?',
                   text: '<p class="p200">Eftervården efter en aknebehandling hos Acnespecialisten är lika viktig som själva behandlingen. När aknen är utklämd och borta är det viktigt att förebygga uppkomsten av ny akne. Om du inte följer din eftervårdsplan kommer du att få resultat, men det kommer inte vara bestående, eftersom akneproduktionen sätter igång igen kort tid efter behandlingen.</p>
 
                   <p class="p200 mt-m">För att hålla huden i kontroll och balans är det viktigt att följa de råd och rekommendationer vi ger dig angående hudvårdsrutin och produkter. Kontinuerlig behandling och förebyggande åtgärder är avgörande för att uppnå och bibehålla en klar och frisk hud. Kom ihåg att vi på Acnespecialisten alltid finns här för att hjälpa och stötta dig genom hela processen, både under och efter din behandling.</p>',
-            ),      
+            ),
+      ),
 );
 $skin_guide_articles = array(
       new SkinGuideArticle(
