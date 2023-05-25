30f49b7cc9ec3c3320f6a72fd246418aff24f503
Patrick Minogue <patrick.minogue@gmail.com>
2023-05-25 08:19:57 +0000

    made scw-content height consistent

--- problem/widgets/symptom-card/symptom-card.php
+++ problem/widgets/symptom-card/symptom-card.php
@@ -8,11 +8,11 @@
                 <?php echo $symptom->name ?>
             </h3>
             <?php if (isset($symptom->aka)) { ?>
-                <p class="p200" id="aka"><?php echo $symptom->aka ?></p>
+                <p class="p200" class="scw-aka"><?php echo $symptom->aka ?></p>
             <?php } ?>
         </div>
     </div>
-    <div class="mt-m p200">
+    <div class="mt-m p200 scw-content">
         <?php echo $symptom->content ?>
     </div>
     <?php if (isset($symptom->url) && isset($symptom->url_label)) { ?>
