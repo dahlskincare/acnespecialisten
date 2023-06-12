42fda2bc25636a8c76d3fd8c757bb0239433d908
Patrick Minogue <patrick.minogue@gmail.com>
2023-06-12 18:55:37 +0000

    added brand urls

--- problem/akne/index.php
+++ problem/akne/index.php
@@ -793,49 +793,65 @@ $brands = array(
             label: 'Powerlite',
             image: 'images/brands/powerlite.svg',
             image_alt: 'Powerlite',
-            image_title: 'Powerlite'
+            image_title: 'Powerlite',
+            url: 'varumarken/powerlite',
+            url_title: 'Powerlite',
       ),
       new Brand(
             label: 'Dermapen4',
             image: 'images/brands/dermapen4.svg',
             image_alt: 'Dermapen4',
-            image_title: 'Dermapen4'
+            image_title: 'Dermapen4',
+            url: 'varumarken/dermapen4',
+            url_title: 'Dermapen4',
       ),
       new Brand(
             label: 'Cryopen',
             image: 'images/brands/cryopen.svg',
             image_alt: 'Cryopen',
-            image_title: 'Cryopen'
+            image_title: 'Cryopen',
+            url: 'varumarken/cryopen',
+            url_title: 'Cryopen',
       ),
       new Brand(
             label: 'Lumenis',
             image: 'images/brands/lumenis.svg',
             image_alt: 'Lumenis',
-            image_title: 'Lumenis'
+            image_title: 'Lumenis',
+            url: 'varumarken/lumenis',
+            url_title: 'Lumenis',
       ),
       new Brand(
             label: 'Alma',
             image: 'images/brands/alma.svg',
             image_alt: 'Alma',
-            image_title: 'Alma'
+            image_title: 'Alma',
+            url: 'varumarken/alma',
+            url_title: 'Alma',
       ),
       new Brand(
             label: 'PRXT',
             image: 'images/brands/prxt.svg',
             image_alt: 'PRXT',
-            image_title: 'PRXT'
+            image_title: 'PRXT',
+            url: 'varumarken/prxt',
+            url_title: 'PRXT',
       ),
       new Brand(
             label: 'Hydrafacial',
             image: 'images/brands/hydrafacial.svg',
             image_alt: 'Hydrafacial',
-            image_title: 'Hydrafacial'
+            image_title: 'Hydrafacial',
+            url: 'varumarken/hydrafacial',
+            url_title: 'Hydrafacial',
       ),
       new Brand(
             label: 'IP infusion',
             image: 'images/brands/ipinfusion.svg',
             image_alt: 'IP infusion',
-            image_title: 'IP infusion'
+            image_title: 'IP infusion',
+            url: 'varumarken/ip-infusion',
+            url_title: 'IP infusion',
       ),
 );
 ?>
