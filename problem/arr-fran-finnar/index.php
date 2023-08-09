5a1aa1f28041bd349bd736dbb4ad8053233f3dbe
Mac <68950292+macentoshMD@users.noreply.github.com>
2023-08-09 23:23:57 +0200

    Update index.php

--- problem/arr-fran-finnar/index.php
+++ problem/arr-fran-finnar/index.php
@@ -537,37 +537,37 @@ $skinguide_url_title = "Läs fler guider om ärr från finnar";
 $specialists = array(
       new Specialist(
             name: 'Cazzandra Lindberg',
-            title: 'Hudterapeut med fokus på akneärr sedan 2015',
+            title: 'Hudterapeut specialiserad på behandling av ärr orsakade av finnar sedan 2015',
             image: 'images/specialists/312x312/hudterapeut-cazzandra.webp',
-            image_alt: 'Cazzandra Lindberg, hudterapeut med specialisering i akneärrbehandling',
-            image_title: 'Cazzandra Lindberg - Expert på akneärrbehandling'
+            image_alt: 'Cazzandra Lindberg, hudterapeut med expertis i ärr av finnar',
+            image_title: 'Cazzandra Lindberg - Expert på behandling av ärr från finnar'
       ),
       new Specialist(
             name: 'Veronika Benedik',
-            title: 'Hudterapeut med specialisering i akneärr sedan 1999',
+            title: 'Erfaren hudterapeut inriktad på ärr som uppstått från finnar sedan 1999',
             image: 'images/specialists/312x312/hudterapeut-veronika.webp',
-            image_alt: 'Veronika Benedik, erfaren hudterapeut med fokus på akneärr',
-            image_title: 'Veronika Benedik - Specialist inom akneärrvård'
+            image_alt: 'Veronika Benedik, erfaren hudterapeut fokuserad på ärr av finnar',
+            image_title: 'Veronika Benedik - Specialist inom behandling av ärr som uppstått av finnar'
       ),
       new Specialist(
             name: 'Amira Maqboul',
-            title: 'Hudterapeut med inriktning mot akneärr sedan 2017',
+            title: 'Hudterapeut med inriktning mot ärr bildade av finnar sedan 2017',
             image: 'images/specialists/312x312/hudterapeut-amira.webp',
-            image_alt: 'Amira Maqboul, hudterapeut specialiserad på akneärrhantering',
-            image_title: 'Amira Maqboul - Hudterapeut specialiserad på akneärr'
+            image_alt: 'Amira Maqboul, hudterapeut specialiserad på behandling av ärr orsakade av finnar',
+            image_title: 'Amira Maqboul - Hudterapeut specialiserad på ärr från finnar'
       ),
       new Specialist(
             name: 'Vilma Libom',
-            title: 'Hudterapeut med specialisering i akneärr sedan 2019',
+            title: 'Hudterapeut med expertis inom ärr som uppstått genom finnar sedan 2019',
             image: 'images/specialists/312x312/hudterapeut-vilma.webp',
-            image_alt: 'Vilma Libom, hudterapeut med inriktning mot akneärrbehandling',
-            image_title: 'Vilma Libom - Expert inom akneärrbehandling'
+            image_alt: 'Vilma Libom, hudterapeut med inriktning mot behandling av ärr bildade av finnar',
+            image_title: 'Vilma Libom - Expert inom behandling av ärr orsakade av finnar'
       )
 );
 
-
 $specialist_url_label = "Se alla specialister";
-$specialist_url_title = "Specialisserade Hudterapeuter med inriktning mot akneärr";
+$specialist_url_title = "Specialiserade Hudterapeuter med inriktning mot ärr bildade av finnar";
+
 
 $related_problems = array(
       new RelatedProblem(
@@ -667,7 +667,7 @@ $brands = array(
 );
 
 $brands_url_label = "Se alla varumärken";
-$brands_url_title = "Varumärken för akneärr";
+$brands_url_title = "Varumärken för ärr";
 
 ?>
 
