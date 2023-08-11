9f74859a0d70fc25f945d72176952726f0b35847
Mac <68950292+macentoshMD@users.noreply.github.com>
2023-08-11 15:43:49 +0200

    Update index.php

--- problem/pigmentflackar/index.php
+++ problem/pigmentflackar/index.php
@@ -167,7 +167,7 @@ $symptoms = array(
       ),
             new Symptom(
             name: 'Solfläckar (Solar Lentigines)',
-            aka: 'även känt som Solskadad hud (Actinic Keratosis)',
+            aka: 'Känt som Solskadad hud (Actinic Keratosis)',
             content: 'Solskadad hud, eller Actinic Keratosis, sker när huden utsätts för alltför mycket solstrålning utan lämpligt skydd. Detta kan leda till en rad olika förändringar i huden, inklusive ålderfläckar, solfläckar (Solar Lentigines), och i värre fall även hudcancer. Solskador kan vara permanenta, men det finns behandlingar som kan ta bort pigmenfläckarna.',
             url: 'problem/solskador/solskadad-hud',
             url_title: 'Lär dig mer om solskadad hud och solfläckar',
@@ -178,7 +178,7 @@ $symptoms = array(
       ),
       new Symptom(
             name: 'Åldersfläckar',
-            aka: 'även känt som åldrande hud (Lentigo Senilis)',
+            aka: 'Känt som åldrande hud (Lentigo Senilis)',
             content: 'Solskadad hud, eller Actinic Keratosis, sker när huden utsätts för alltför mycket solstrålning utan lämpligt skydd. Detta kan leda till en rad olika förändringar i huden, inklusive ålderfläckar, solfläckar (Solar Lentigines), och i värre fall även hudcancer. Solskador kan vara permanenta, men det finns behandlingar som kan ta bort pigmenfläckarna.',
             url: 'problem/solskador/solskadad-hud',
             url_title: 'Lär dig mer om solskadad hud och solfläckar',
@@ -188,8 +188,8 @@ $symptoms = array(
             image_title: 'Solskadad hud'
       ),
       new Symptom(
-            name: 'Röda / Lila Pigmenteringar',
-            aka: 'känt som Postinflammatorisk Hyperpigmentering (PIH)',
+            name: 'Mörka / Lila Pigmenteringar',
+            aka: 'Känt som Postinflammatorisk Hyperpigmentering (PIH)',
             content: 'Mörka fläckar efter akne eller inflammation. PIH uppstår som ett resultat av inflammation, inklusive akne. Ökad melaninproduktion leder till mörkare märken som kan variera i färg.',
             url: 'problem/pigmentflackar/postinflammatorisk-hyperpigmentering',
             url_title: 'Lär dig mer om Postinflammatorisk Hyperpigmentering',
@@ -198,19 +198,8 @@ $symptoms = array(
             image_alt: 'Bild som visar Postinflammatorisk Hyperpigmentering',
             image_title: 'Postinflammatorisk Hyperpigmentering'
       ),
-      new Symptom(
-            name: 'Fräknar',
-            aka: 'Vetenskapligt kända som Ephelides',
-            content: 'Fräknar är små bruna fläckar som oftast förekommer på ljus hud. De är ofarliga och är vanligtvis ett resultat av genetik och exponering för solen.',
-            url: 'problem/pigmentflackar/freckles',
-            url_title: 'Lär dig mer om Fräknar',
-            url_label: 'Läs mer',
-            image: 'images/symptoms/102x102/freckles.webp',
-            image_alt: 'Bild som visar Fräknar',
-            image_title: 'Fräknar'
-      ),
-      new Symptom(
-            name: 'Ljusa / Vita pigmentfläckar',
+            new Symptom(
+            name: 'Vita pigmentfläckar',
             aka: 'känt som Vitiligo',
             content: 'Vitiligo orsakar förlust av hudfärg i fläckar och kan påverka alla delar av huden. Det är en autoimmun sjukdom, och de exakta orsakerna är okända.',
             url: 'problem/pigmentflackar/vitiligo',
@@ -220,6 +209,29 @@ $symptoms = array(
             image_alt: 'Bild som visar Vitiligo',
             image_title: 'Vitiligo'
       ),
+            new Symptom(
+            name: 'Ljusbruna Pigmentfläckar',
+            aka: 'även känt som Café au lait-fläckar',
+            content: 'Ljusbruna pigmentfläckar, eller Café au lait-fläckar, är ljusbruna till mörkbruna jämnt färgade fläckar på huden. De är vanligtvis ovala i formen och kan förekomma var som helst på kroppen. Café au lait-fläckar är oftast ofarliga men kan ibland vara associerade med vissa genetiska tillstånd som Neurofibromatos.',
+            url: 'problem/pigmentflackar/cafe-au-lait',
+            url_title: 'Lär dig mer om Café au lait-fläckar',
+            url_label: 'Läs mer',
+            image: 'images/symptoms/102x102/cafe-au-lait.webp',
+            image_alt: 'Bild som visar Café au lait-fläckar',
+            image_title: 'Café au lait-fläckar'
+      ),
+      new Symptom(
+            name: 'Fräknar',
+            aka: 'Känt som Ephelides',
+            content: 'Fräknar är små bruna fläckar som oftast förekommer på ljus hud. De är ofarliga och är vanligtvis ett resultat av genetik och exponering för solen.',
+            url: 'problem/pigmentflackar/freckles',
+            url_title: 'Lär dig mer om Fräknar',
+            url_label: 'Läs mer',
+            image: 'images/symptoms/102x102/freckles.webp',
+            image_alt: 'Bild som visar Fräknar',
+            image_title: 'Fräknar'
+      ),
+
 
 
 
