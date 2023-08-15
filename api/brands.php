b97c23fa1b04787f1ad5a1214a170e3d897fd504
Patrick Minogue <patrick.minogue@gmail.com>
2023-08-15 10:47:31 +0000

    added endpoints to problems/brands/services

---
+++ api/brands.php
@@ -0,0 +1,34 @@
+<?php
+
+// add http headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$servername = "db";
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+$query = "SELECT * FROM $dbname.brand LIMIT 1000";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("DB connection failed: " . mysqli_connect_error());
+}
+
+$result = mysqli_query($conn, $query);
+if ($result == false) {
+    http_response_code(500);
+} else {
+
+    http_response_code(200);
+    $output = array();
+    foreach ($result as $row) {
+        $output[] = $row;
+    }
+
+    echo json_encode($output, JSON_UNESCAPED_SLASHES);
+}
