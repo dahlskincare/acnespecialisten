7cfc885743e70c3f971f67cb1d3ca97655c22aa1
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2023-12-11 15:32:16 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -13,6 +13,7 @@
             <a href="behandlingar" class="mobile-nav-link l10n" title="Behandlingar">Behandlingar</a>
             <a href="resultat" class="mobile-nav-link l10n" title="Resultat">Resultat</a>
             <a href="varumarken" class="mobile-nav-link l10n" title="Varumärken">Varumärken</a>
+            <li><a href="om-oss" class="h100 l10n" title="Om oss">Om oss</a></li>
             <a href="kontakt" class="mobile-nav-link l10n" title="Kontakt">Kontakt</a>
         </div>
         <div id="mobile-expansion-panels">
