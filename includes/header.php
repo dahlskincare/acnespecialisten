e81940cfde221e925e5cd3c50658b2c0bb99f24f
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2023-12-19 15:56:40 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -23,8 +23,8 @@
                     <?php icon('arrow-down') ?>
                 </div>
                 <div class="ep-content">
-                    <a href="om-oss" class="l10n" title="Om oss">Om oss</a>
-                    <a href="metod" class="l10n" title="Vår metod">Vår metod</a>
+                    <a href="om-oss.php" class="l10n" title="Om oss">Om oss</a>
+                    <a href="metod.php" class="l10n" title="Vår metod">Vår metod</a>
                     <a href="specialister" class="l10n" title="Våra specialister">Våra specialister</a>
                     <a href="https://skonhetsgruppen.teamtailor.com" rel="nofollow" title="Teamtailor" target="_blank" class="l10n">Karriär</a>
                 </div>
@@ -35,10 +35,10 @@
                     <?php icon('arrow-down') ?>
                 </div>
                 <div class="ep-content">
-                    <a href="faq" class="l10n" title="Frågor & Svar">Frågor & Svar</a>
-                    <a href="ordlista" class="l10n" title="Ordlista">Ordlista</a>
+                    <a href="fragor-svar.php" class="l10n" title="Frågor & Svar">Frågor & Svar</a>
+                    <a href="ordlista.php" class="l10n" title="Ordlista">Ordlista</a>
                     <!--<a href="hudguide" class="l10n" title="Hudguide">Hudguide</a>-->
-                    <a href="kontakt" class="l10n" title="Kontakt">Kontakt</a>
+                    <a href="kontakt.php" class="l10n" title="Kontakt">Kontakt</a>
                 </div>
             </div>
             <div class="expansion-panel">
@@ -47,9 +47,9 @@
                     <?php icon('arrow-down') ?>
                 </div>
                 <div class="ep-content">
-                    <a href="resultat" class="l10n" title="Resultat">Resultat</a>
-                    <a href="presentkort" class="l10n" title="Presentkort">Presentkort</a>
-                    <a href="gratis-hudkonsultation" class="l10n" title="Gratis konsultation">Gratis konsultation</a>
+                    <a href="resultat.php" class="l10n" title="Resultat">Resultat</a>
+                    <a href="presentkort.php" class="l10n" title="Presentkort">Presentkort</a>
+                    <a href="gratis-hudkonsultation.php" class="l10n" title="Gratis konsultation">Gratis konsultation</a>
                 </div>
             </div>
         </div>
@@ -72,8 +72,8 @@
                     <div class="column">
                         <ul>
                             <li class="l10n">Företag</li>
-                            <li><a href="om-oss" class="l10n" title="Om oss">Om oss</a></li>
-                            <li><a href="metod" class="l10n" title="Vår metod">Vår metod</a></li>
+                            <li><a href="om-oss.php" class="l10n" title="Om oss">Om oss</a></li>
+                            <li><a href="metod.php" class="l10n" title="Vår metod">Vår metod</a></li>
                             <li><a href="specialister" class="l10n" title="Våra specialister">Våra specialister</a></li>
                             <li><a href="https://skonhetsgruppen.teamtailor.com" target="_blank" rel="nofollow" title="Teamtailor" class="l10n">Karriär</a></li>
                         </ul>
@@ -81,19 +81,19 @@
                     <div class="column">
                         <ul>
                             <li class="l10n">Kundtjänst</li>
-                            <li><a href="faq" class="l10n" title="Frågor & Svar">Frågor & Svar</a></li>
-                            <li><a href="avbokningspolicy" class="l10n" title="Avbokning">Avbokning</a></li>
-                            <li><a href="ordlista" class="l10n" title="Ordlista">Ordlista</a></li>
+                            <li><a href="fragor-svar.php" class="l10n" title="Frågor & Svar">Frågor & Svar</a></li>
+                            <li><a href="avbokningspolicy.php" class="l10n" title="Avbokning">Avbokning</a></li>
+                            <li><a href="ordlista.php" class="l10n" title="Ordlista">Ordlista</a></li>
                             <!--<li><a href="hudguide" class="l10n" title="Hudguide">Hudguide</a></li>-->
-                            <li><a href="kontakt" class="l10n" title="Kontakt">Kontakt</a></li>
+                            <li><a href="kontakt.php" class="l10n" title="Kontakt">Kontakt</a></li>
                         </ul>
                     </div>
                     <div class="column">
                         <ul>
                             <li class="l10n">Övrigt</li>
-                            <li><a href="resultat" class="l10n" title="Resultat">Resultat</a></li>
-                            <li><a href="presentkort" class="l10n" title="Presentkort">Presentkort</a></li>
-                            <li><a href="gratis-hudkonsultation" class="l10n" title="Gratis konsultation">Gratis konsultation</a></li>
+                            <li><a href="resultat.php" class="l10n" title="Resultat">Resultat</a></li>
+                            <li><a href="presentkort.php" class="l10n" title="Presentkort">Presentkort</a></li>
+                            <li><a href="gratis-hudkonsultation.php" class="l10n" title="Gratis konsultation">Gratis konsultation</a></li>
                         </ul>
                     </div>
                     <div class="column">
@@ -134,17 +134,17 @@
                 </a>
                 <nav>
                     <ul>
-                        <li><a href="problem" class="h100 l10n" title="Problem">Problem</a></li>
-                        <li><a href="behandlingar" class="h100 l10n" title="Behandlingar">Behandlingar</a></li>
+                        <li><a href="problem.php" class="h100 l10n" title="Problem">Problem</a></li>
+                        <li><a href="behandlingar.php" class="h100 l10n" title="Behandlingar">Behandlingar</a></li>
                         <li><a href="varumarken" class="h100 l10n" title="Varumärken">Varumärken</a></li>
-                        <li><a href="resultat" class="h100 l10n" title="Resultat">Resultat</a></li>
-                        <li><a href="om-oss" class="h100 l10n" title="Om ossr">Om oss</a></li>
-                        <li><a href="kontakt" class="h100 l10n" title="Kontakt">Kontakt</a></li>
+                        <li><a href="resultat.php" class="h100 l10n" title="Resultat">Resultat</a></li>
+                        <li><a href="om-oss.php" class="h100 l10n" title="Om ossr">Om oss</a></li>
+                        <li><a href="kontakt.php" class="h100 l10n" title="Kontakt">Kontakt</a></li>
                     </ul>
                 </nav>
             </div>
             <div id="actions">
-                <a class="button text l10n" href="gratis-hudkonsultation" title="Boka gratis konsultation">Boka gratis konsultation</a>
+                <a class="button text l10n" href="gratis-hudkonsultation.php" title="Boka gratis konsultation">Boka gratis konsultation</a>
                 <div onclick="HeaderDesktop.toggleMenu()">
                     <?php icon("bars") ?>
                 </div>
