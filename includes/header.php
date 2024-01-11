367bf6a724dc8a29c78d28d000d03e285c6be76f
Patrick Minogue <patrick.minogue@gmail.com>
2024-01-11 13:33:01 +0000

    fix "about us" design (fix: #28)

--- includes/header.php
+++ includes/header.php
@@ -9,11 +9,10 @@
         </div>
         <div id="mobile-nav-links">
             <a href="problem" class="mobile-nav-link l10n" title="Problem">Problem</a>
-            <!--<a href="priser" class="mobile-nav-link l10n" title="Priser">Priser</a>-->
             <a href="behandlingar" class="mobile-nav-link l10n" title="Behandlingar">Behandlingar</a>
             <a href="resultat" class="mobile-nav-link l10n" title="Resultat">Resultat</a>
             <a href="varumarken" class="mobile-nav-link l10n" title="Varumärken">Varumärken</a>
-            <li><a href="om-oss" class="h100 l10n" title="Om oss">Om oss</a></li>
+            <a href="om-oss" class="mobile-nav-link l10n" title="Om oss">Om oss</a>
             <a href="kontakt" class="mobile-nav-link l10n" title="Kontakt">Kontakt</a>
         </div>
         <div id="mobile-expansion-panels">
