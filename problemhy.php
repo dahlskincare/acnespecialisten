222c5812d63efe3461dc241abb933982296e2625
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-01-23 13:23:12 +0100

    Update problemhy.php

--- problemhy.php
+++ problemhy.php
@@ -352,7 +352,7 @@ $problem_areas = array(
             problem_ids: array('akne', 'aknearr', 'arr-fran-finnar', 'bristningar', 'finnar', 'hudforandringar', 'mogen-hy', 'oonskat-har', 'operations-skadearr', 'pormaskar', 'stora-porer', 'blandhy', 'seborre', 'torr-kanslig-hy', 'ytliga-blodkarl', 'pigmentflackar',)
       ),
 );
-
+require_once("includes/models/path_segment.php");
 $path_segments = array(
       new PathSegment('Problem', '/problemhy.php'),
 );
