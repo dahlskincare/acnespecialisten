bdf5a7c07f23b75aa4347917d5c23a5c7f9de4ec
Patrick Minogue <patrick.minogue@gmail.com>
2024-01-27 03:37:27 +0000

    make product->image nullable

--- problem/widgets/product-card/product-card.php
+++ problem/widgets/product-card/product-card.php
@@ -1,10 +1,12 @@
 <div class="product-card-widget">
-    <picture>
+    <?php if(isset($product->image_small) && isset($product->image_large)) { ?>
+    <picture class="mb-xl">
         <source media="(max-width: 799px)" srcset="<?php echo $product->image_small ?>">
         <source media="(min-width: 800px)" srcset="<?php echo $product->image_large ?>">
         <img src="<?php echo $product->image_large ?>" alt="<?php echo $product->image_alt ?>" title="<?php echo $product->image_title ?>" width="358" height="274" />
     </picture>
-    <div class="mt-xl h200"><?php echo $product->price ?></div>
+    <?php } ?>
+    <div class="h200"><?php echo $product->price ?></div>
     <div class="mt-m">
         <?php echo $product->content ?>
     </div>
