778c43707f17f99a14dfe69a46b054d280ffe8f4
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-02-02 15:34:45 +0100

    Update metoden.php

--- metoden.php
+++ metoden.php
@@ -6,32 +6,32 @@ $steps = array(
     new StepModel(
         title: '01. Identifiera',
         content: "På Acnespecialisten inleder vi din hudvårdsresa med en gratis hudkonsultation. Under denna session identifierar våra experter din hudtyp och eventuella hudproblem. Denna noggranna analys är grunden för att skapa en effektiv och personligt anpassad behandlingsplan.",
-        image_small: 'bilder/behandlingar/358x272/konsultation.webp',
-        image_large: 'bilder/behandlingar/424x526/konsultation.webp',
+        image_small: 'bilder/behandlingar/358x274/konsultation.webp',
+        image_large: 'bilder/behandlingar/424x456/konsultation.webp',
         url: 'gratis-hudkonsultation.php',
         url_label: 'Boka en gratis konsultation'
     ),
     new StepModel(
         title: '02. Behandla',
         content: "Efter identifieringen av dina hudproblem, tar vi nästa steg mot att behandla dem direkt. Våra specialister använder avancerade tekniker och behandlingsmetoder för att effektivt hantera dina specifika hudproblem och uppnå synliga resultat.",
-        image_small: 'bilder/behandlingar/358x272/ansiktsbehandlingar-mot-problemhy.webp',
-        image_large: 'bilder/behandlingar/424x526/ansiktsbehandlingar-mot-problemhy.webp',
+        image_small: 'bilder/behandlingar/358x274/ansiktsbehandlingar-mot-problemhy.webp',
+        image_large: 'bilder/behandlingar/424x456/ansiktsbehandlingar-mot-problemhy.webp',
         url: 'behandlingar.php',
         url_label: 'Se våra behandlingar'
     ),
     new StepModel(
         title: '03. Förebygga',
         content: "För att säkerställa långsiktiga resultat fokuserar vi på att förebygga framtida hudproblem. Vi ger dig skräddarsydda råd och rekommenderar högkvalitativa hudvårdsprodukter som är anpassade för att skydda och upprätthålla din huds hälsa.",
-        image_small: 'bilder/behandlingar/358x272/produkter.webp',
-        image_large: 'bilder/behandlingar/424x526/produkter.webp',
+        image_small: 'bilder/behandlingar/358x274/produkter.webp',
+        image_large: 'bilder/behandlingar/424x456/produkter.webp',
         url: 'https://dahlskincare.se',
         url_label: 'Upptäck våra produkter'
     ),
     new StepModel(
         title: '04. Reparera',
         content: "I det sista steget i vår metod tar vi hand om eventuella återstående effekter av hudproblem, såsom ärr och pigmentförändringar. Vi erbjuder behandlingar och vård för att reparera och återställa huden, och ge dig en hälsosam och strålande hy.",
-        image_small: 'bilder/behandlingar/358x272/microneedling.webp',
-        image_large: 'bilder/behandlingar/424x526/microneedling.webp',
+        image_small: 'bilder/behandlingar/358x274/microneedling.webp',
+        image_large: 'bilder/behandlingar/424x456/microneedling.webp',
         url: 'behandlingar.php',
         url_label: 'Läs mer om eftervård'
     ),
