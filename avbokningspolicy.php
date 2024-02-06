578507abf5cf0b612888b9c28bb911171ae8fc2f
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-02-06 15:53:27 +0100

    Update avbokningspolicy.php

--- avbokningspolicy.php
+++ avbokningspolicy.php
@@ -52,68 +52,7 @@
                     ?>
                 </section>
             </div>
-            <section id="results" class="large-margin">
-                <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/widgets/results/results.php') ?>
-            </section>
-            <section id="reviews" class="large-margin">
-                <div class="flex-row align-end justify-space-between">
-                    <div class="h500 l10n">Omdömen</div>
-                    <div class="flex-row is-hidden-mobile">
-                        <div class="step-buttons">
-                            <button class="round-large grey" onclick="Reviews.scroll(-1)">
-                                <?php icon('arrow-left') ?>
-                            </button>
-                            <button class="round-large grey" onclick="Reviews.scroll(1)">
-                                <?php icon('arrow-right') ?>
-                            </button>
-                        </div>
-                        <a href="https://se.trustpilot.com/review/acnespecialisten.se" title="Omdömen" class="ml-l button compact text">
-                            <span class="l10n">Omdömen</span>
-                            <?php icon('navigate-next') ?>
-                        </a>
-                    </div>
-
-                </div>
-                <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/widgets/reviews/reviews.php'); ?>
-                <a class="mt-xl button b200 outline expand is-hidden-tablet l10n" href="https://se.trustpilot.com/review/acnespecialisten.se" title="Se alla omdömen">Se alla omdömen</a>
-            </section>
-            <section id="faq" class="large-margin">
-                <div class="flex-row align-end justify-space-between">
-                    <h2 class="big l10n">Frågor & Svar</h2>
-                    <a href="fragor-svar.php" class="button compact text is-hidden-mobile">
-                        <span class="l10n">Se alla frågor</span>
-                        <?php icon('navigate-next') ?>
-                    </a>
-                </div>
-                <?php
-                $faq_categories = null;
-                include($_SERVER['DOCUMENT_ROOT'] . '/includes/widgets/faq/faq.php');
-                ?>
-                <a class="mt-xl button outline expand is-hidden-tablet l10n" href="fragor-svar.php" title="Se alla frågor">Se alla frågor</a>
-            </section>
-            <!--Hudguide-->
-            <section id="specialists" class="large-margin">
-                <div class="flex-row align-end justify-space-between">
-                    <h2 class="big l10n">Våra specialsiter</h2>
-                    <a href="specialister" class="button compact text is-hidden-mobile">
-                        <span class="l10n">Se alla specialister</span>
-                        <?php icon('navigate-next') ?>
-                    </a>
-                </div>
-                <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/widgets/specialists/specialists.php'); ?>
-                <a class="mt-xl button outline expand is-hidden-tablet l10n" href="specialister" title="Se alla specialister">Se alla specialister</a>
-            </section>
-            <section id="brands" class="large-margin">
-                <div class="flex-row align-end justify-space-between">
-                    <div class="h500 l10n">Varumärken</div>
-                    <a href="varumarken" class="button compact text is-hidden-mobile">
-                        <span class="l10n">Se alla varumärken</span>
-                        <?php icon('navigate-next') ?>
-                    </a>
-                </div>
-                <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/widgets/brands/brands.php'); ?>
-                <a class="mt-xl button b200 outline expand is-hidden-tablet l10n" href="varumarken" title="Se alla varumärken">Se alla varumärken</a>
-            </section>
+            
         </div>
     </main>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
