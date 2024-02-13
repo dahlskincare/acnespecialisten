60091eb3bb861f8bdd115ca4317d840c2ade1afb
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-02-13 14:20:33 +0100

    Update finnar-arr.php

--- finnar-arr.php
+++ finnar-arr.php
@@ -419,7 +419,7 @@ $faq_categories = array(
             ),
             new Question(
                   title: 'Är det smärtsamt att behandla ärr från finnar? Hur ser eftervård ut?',
-                  text: '<p class="p200">Vi gör vårt bästa för att minimera obehag. För våra mest smärtsamma behandlingar erbjuder vi en bedövningskräms så att du inte känner något. Eftervården är anpassad till varje individ och du får fullständiga instruktioner för hudvård efter varje behandling.</p>'
+                  text: '<p class="p200">Vi gör vårt bästa för att minimera obehag. För våra mest smärtsamma behandlingar erbjuder vi en bedövningskräm så att du inte känner något. Eftervården är anpassad till varje individ och du får fullständiga instruktioner för hudvård efter varje behandling.</p>'
             ),
             new Question(
                   title: 'Vilken tid behöver jag mellan behandlingar? När kommer jag att se förbättringar?',
