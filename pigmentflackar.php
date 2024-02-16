2333743c3b5d007c4f7083f989a6f6a07ce6861a
Peter Engström <peter.engstrom@dahlskincare.com>
2024-02-16 16:48:37 +0100

    Update pigmentflackar.php

--- pigmentflackar.php
+++ pigmentflackar.php
@@ -291,7 +291,7 @@ $services = array(
             title: 'IPL-behandling för Pigmentfläckar',
             duration: '60 min',
             price: '2595 kr',
-            content: 'IPL (Intense Pulsed Light) utnyttjar fokuserade ljusstrålar för att behandla och reducera pigmentfläckar och pigmenteringar. Behandlingen främjar hudens kollagenproduktion och tar bort skadade hudceller, vilket resulterar i en jämnare hud och mindre synliga fläckar.',
+            content: 'IPL utnyttjar fokuserade ljusstrålar för att behandla och reducera pigmentfläckar och pigmenteringar. Behandlingen främjar hudens kollagenproduktion och tar bort skadade hudceller, vilket resulterar i en jämnare hud och mindre synliga fläckar.',
             image_small: 'images/services/200x200/ipl.webp',
             image_large: 'images/services/200x200/ipl.webp',
             image_alt: 'Bild av en IPL-behandling för pigmentfläckar',
