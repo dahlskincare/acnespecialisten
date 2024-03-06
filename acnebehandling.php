aa97e4e83388008f2b5b3b0c09089e92e881a5b6
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-03-06 15:05:45 +0100

    Update acnebehandling.php

--- acnebehandling.php
+++ acnebehandling.php
@@ -172,7 +172,7 @@ $products = array(
 );
 
 $headline04 = '04. Reparera post-akne (Acneärr)';
-$repair_content = '<p class="p200">När din akne har behandlats och aknen tagits bort, kan det hända att den lämnar ärr bakom sig. På Acnespecialisten kan vi hjälpa dig att reparera dessa ärr genom olika tekniker som laserteknik och microneedling. Dessa metoder främjar hudens egen regenerering, hjälper till att återställa en jämn hudstruktur och minska synligheten av ärr. Med oss vid din sida kan du känna att du har stödet du behöver för att ta bort akne och njuta av en klarare, friskare hy.</p>';
+$repair_content = '<p class="p200">När din akne har behandlats och aknen försvunnit, tar vi itu med akneärren som den lämnat bakom sig. På Acnespecialisten kan vi hjälpa dig att reparera dessa ärr genom olika tekniker som laserteknik och microneedling. Dessa metoder främjar hudens egen regenerering, hjälper till att återställa en jämn hudstruktur och minska synligheten av ärr. Med oss vid din sida kan du känna att du har stödet du behöver för att ta bort akne och njuta av en klarare, friskare hy.</p>';
 
 $repair_url = 'behandla-acnearr.php';
 $repair_url_label = 'Se ärrbehandlingar';
