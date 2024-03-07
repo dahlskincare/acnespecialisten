56b06631d4360870db7532ed1c4dfa1ab4ea07de
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-03-07 12:34:36 +0100

    Update behandla-acnearr.php

--- behandla-acnearr.php
+++ behandla-acnearr.php
@@ -144,8 +144,10 @@ $results = array(
             image_after_small: '/bilder/resultat/358x358/resultat-akne-efter-1.jpg',
             image_before_large: '/bilder/resultat/424x424/resultat-akne-fore-1.jpg',
             image_after_large: '/bilder/resultat/424x424/resultat-akne-efter-1.jpg',
-            image_title: 'Person som har akne',
-            image_alt: 'Ansiktet på en person som behandlat svår akne, före och efterbild',
+            image_before_title: 'Person som har akne',
+            image_before_alt: 'Ansiktet på en person som behandlat svår akne, förebild',
+            image_after_title: 'Person som är aknefri',
+            image_after_alt: 'Ansiktet på en person som behandlat svår akne och blivit av med den, efterbild',
             treatment: new ResultTreatment(
                   duration: '<a href="acne.php" title="Svår akne">Svår akne</a>',
             )
@@ -155,8 +157,10 @@ $results = array(
             image_after_small: '/bilder/resultat/358x358/resultat-akne-efter-2.jpg',
             image_before_large: '/bilder/resultat/424x424/resultat-akne-fore-2.jpg',
             image_after_large: '/bilder/resultat/424x424/resultat-akne-efter-2.jpg',
-            image_title: 'Person som har akne',
-            image_alt: 'Ansiktet på en person som behandlat akne, före och efterbild',
+            image_before_title: 'Person som har akne',
+            image_before_alt: 'Ansiktet på en person som behandlat akne, förebild',
+            image_after_title: 'Person som är aknefri',
+            image_after_alt: 'Ansiktet på en person som behandlat akne och blivit av med den, efterbild',
             treatment: new ResultTreatment(
                   duration: '<a href="acne.php" title="Akne">Akne</a>',
             )
@@ -166,8 +170,10 @@ $results = array(
             image_after_small: '/bilder/resultat/358x358/resultat-rosacea-efter.jpg',
             image_before_large: '/bilder/resultat/424x424/resultat-rosacea-fore.jpg',
             image_after_large: '/bilder/resultat/424x424/resultat-rosacea-efter.jpg',
-            image_title: 'Person som har rosacea',
-            image_alt: 'Ansiktet på en person som behandlat rosacea, före och efterbild',
+            image_before_title: 'Person som har rosacea',
+            image_before_alt: 'Ansiktet på en person som behandlat rosacea, förebild',
+            image_after_title: 'Person som är rosaceafri',
+            image_after_alt: 'Ansiktet på en person som behandlat rosacea och blivit av med den, efterbild',
             treatment: new ResultTreatment(
                   duration: '<a href="rosacea.php" title="Rosacea">Rosacea</a>',
             )
