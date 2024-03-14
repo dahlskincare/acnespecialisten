09b2e14c190acf696bc5f8bbb4db8c1af69e7285
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-03-14 15:08:50 +0100

    Update index.php

--- varumarken/powerlite-photonova/index.php
+++ varumarken/powerlite-photonova/index.php
@@ -80,7 +80,7 @@ $description_text = '<p class="p200">Powerlite är varumärket bakom Photonova, 
 
 $top_articles = array(
     'process' => new Article(
-        title: 'Hur går en Powerlite Photonova behandling till?',
+        title: 'Hur går en IPL laser behandling i ansiktet till?',
         image_small: '/bilder/process/358x272/powerlite-photonova-process.webp',
         image_large: '/bilder/process/872x456/powerlite-photonova-process.webp',
         image_alt: 'Hur går en Rejuve Powerlite Photonova behandling till?',
