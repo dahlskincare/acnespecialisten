3c91d9c04182edafc15eee7ca4d65b494aee71d0
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-04-11 13:08:43 +0200

    Update finnar.php

--- finnar.php
+++ finnar.php
@@ -5,9 +5,9 @@ include_once('hudproblem/widgets/symptom-card/symptom.php');
 include_once('hudproblem/widgets/related-problem-card/related-problem.php');
 include_once($_SERVER['DOCUMENT_ROOT'] . '/includes/models.php');
 
-$seo_title = 'Vad är finnar? - Symptom, orsak och behandling';
-$seo_description = 'Här hittar du allt om finnar och hur man blir av med det. Boka in en kostnadsfri konsultation där vi tar fram en personlig behandlingsplan.';
-$seo_keywords = 'finnar, vad är finnar, orsaker till finnar, varför får man finnar, var finnar, varför finnar';
+$seo_title = 'Vad är finnar och ska man klämma dem?';
+$seo_description = 'Här hittar du allt om finnar och hur man får bort dem. Boka in en kostnadsfri konsultation där vi tar fram en personlig behandlingsplan.';
+$seo_keywords = 'finnar, klämma finnar, klämma acne, klämma finnar ärr, ska man klämma finnar i ansiktet, ta bort finnar, få bort finnar, hur får man bort finnar, bli av med finnar';
 
 $title = 'Finnar';
 
