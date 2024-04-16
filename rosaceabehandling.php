0c20cc6c80076013b8aed8d9a88118415c081a35
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-04-16 12:27:38 +0200

    t

--- rosaceabehandling.php
+++ rosaceabehandling.php
@@ -596,9 +596,9 @@ $brands_url_title = "Varumärken för Rosaceabehandling";
                 <!--Hudguide-->
                 <section id="specialists">
                     <div class="flex-row align-end justify-space-between">
-                        <h2 class="big l10n">Våra specialister</h2>
-                        <a href="specialister" title="Specialister" class="button compact text is-hidden-mobile">
-                            <span class="l10n">Se alla specialister</span>
+                        <h2 class="big l10n">Våra hudterapeuter</h2>
+                        <a href="hudterapeut" title="Hudterapeuter" class="button compact text is-hidden-mobile">
+                            <span class="l10n">Se alla hudterapeuter</span>
                             <?php icon('navigate-next') ?>
                         </a>
                     </div>
