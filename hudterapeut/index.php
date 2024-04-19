dda8ddbd8a7f1a59f55c77897689ff064c041065
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-04-19 15:33:22 +0200

    Update index.php

--- hudterapeut/index.php
+++ hudterapeut/index.php
@@ -118,7 +118,7 @@ $specialists = array(
         description: '',
         rating: 4.6,
         num_treatments: 1099,
-        since_year: 2021,
+        since_year: 2020,
         booking_url: 'https://www.bokadirekt.se/places/sveriges-skonhetscenter-acnespecialisten-sundbyberg-19300?_gl=1%2A2y7n64%2A_ga%2AMTU1NDQ2MjU3MS4xNjYwNTUzMjIy%2A_ga_7EMC778CH1%2AMTcwNzIzMTUyOS4yMjAuMS4xNzA3MjMxNzExLjYwLjAuMA..&sr=366A581A_10F4_4050_9591_4A20DD7082C6',
     ),
     new Specialist(
