542e108e15de7c7cb5df010064ddbb4bef9416d1
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-04-22 10:58:56 +0200

    Update index.php

--- varumarken/index.php
+++ varumarken/index.php
@@ -7,7 +7,6 @@ $green_banner_content = new GreenBannerContent(
       title: 'Varumärken',
       description: 'På AcneSpecialisten är vi dedikerade till att behandla och förbättra hudproblem som akne, rosacea, pormaskar och finnar, och detta åstadkommer vi genom att använda ledande varumärken och tekniker inom hudvård. Våra samarbetspartners inkluderar Alma, Dermapen, CryoPen, HydraFacial, Infuzion, Splendor X och Soprano Ice, som alla erbjuder banbrytande lösningar för en rad hudtillstånd.',
       description_extended: 'Genom dessa innovativa behandlingsmetoder och vår expertis, skapar vi skräddarsydda behandlingsplaner för att möta dina individuella hudvårdsbehov. Om du är osäker på ditt hudtillstånd eller vilken behandling som passar dig bäst, rekommenderar vi en kostnadsfri hudkonsultation hos oss.',
-      button_url: 'gratis-hudkonsultation',
       button_url: 'gratis-hudkonsultation.php',
       button_label: 'Boka gratis konsultation'
 );
