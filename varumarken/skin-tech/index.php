5f85432f845101e5b886964aeda954e1281a3560
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-04-23 16:02:01 +0200

    Update index.php

--- varumarken/skin-tech/index.php
+++ varumarken/skin-tech/index.php
@@ -880,7 +880,7 @@ $specialists_view_more = 'Se alla hudterapeuter';
                             <div class="mb-xl"><?php echo $treatment_area->description ?></div>
                         <?php
                             foreach ($treatment_area->items as $treatment_area_item) {
-                                include($_SERVER['DOCUMENT_ROOT'] . 'hudbehandlingar/widgets/treatment-area-item-card/treatment-area-item-card.php');
+                                include($_SERVER['DOCUMENT_ROOT'] . '/hudbehandlingar/widgets/treatment-area-item-card/treatment-area-item-card.php');
                             }
                         } ?>
                     </section>
