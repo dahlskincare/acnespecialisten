4a23fd37dbf6fb16d6db5af251086e5bfbc9ae45
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-04-25 12:56:20 +0200

    Update finnar-behandling.php

--- finnar-behandling.php
+++ finnar-behandling.php
@@ -185,45 +185,45 @@ $repair_url_label = 'Se ärrbehandlingar efter finnar';
 $repair_url_title = 'klicka här för att se våra ärrbehandlingar efter finnar';
 
 $results = array(
-    new ResultCustomer(
-        image_before_small: '/bilder/resultat/358x358/resultat-akne-fore-1.jpg',
-        image_after_small: '/bilder/resultat/358x358/resultat-akne-efter-1.jpg',
-        image_before_large: '/bilder/resultat/424x424/resultat-akne-fore-1.jpg',
-        image_after_large: '/bilder/resultat/424x424/resultat-akne-efter-1.jpg',
-        image_before_alt: 'Före',
-        image_before_title: 'Före',
-        image_after_alt: 'Efter',
-        image_after_title: 'Efter',
-        treatment: new ResultTreatment(
-            duration: '3 månader',
-        )
-    ),
-    new ResultCustomer(
-        image_before_small: '/bilder/resultat/358x358/resultat-akne-fore-2.jpg',
-        image_after_small: '/bilder/resultat/358x358/resultat-akne-efter-2.jpg',
-        image_before_large: '/bilder/resultat/424x424/resultat-akne-fore-2.jpg',
-        image_after_large: '/bilder/resultat/424x424/resultat-akne-efter-2.jpg',
-        image_before_alt: 'Före',
-        image_before_title: 'Före',
-        image_after_alt: 'Efter',
-        image_after_title: 'Efter',
-        treatment: new ResultTreatment(
-            duration: '2 månader',
-        )
-    ),
-    new ResultCustomer(
-        image_before_small: '/bilder/resultat/358x358/resultat-rosacea-fore.jpg',
-        image_after_small: '/bilder/resultat/358x358/resultat-rosacea-efter.jpg',
-        image_before_large: '/bilder/resultat/424x424/resultat-rosacea-fore.jpg',
-        image_after_large: '/bilder/resultat/424x424/resultat-rosacea-efter.jpg',
-        image_before_alt: 'Före',
-        image_before_title: 'Före',
-        image_after_alt: 'Efter',
-        image_after_title: 'Efter',
-        treatment: new ResultTreatment(
-            duration: '4 månader',
-        )
-    ),
+      new ResultCustomer(
+            image_before_small: '/bilder/resultat/358x358/resultat-akne-fore-1.jpg',
+            image_after_small: '/bilder/resultat/358x358/resultat-akne-efter-1.jpg',
+            image_before_large: '/bilder/resultat/424x424/resultat-akne-fore-1.jpg',
+            image_after_large: '/bilder/resultat/424x424/resultat-akne-efter-1.jpg',
+            image_before_alt: 'Före',
+            image_before_title: 'Före',
+            image_after_alt: 'Efter',
+            image_after_title: 'Efter',
+            treatment: new ResultTreatment(
+                  duration: '<a href="acne.php" title="Svår akne">Svår akne</a>',
+            )
+      ),
+      new ResultCustomer(
+            image_before_small: '/bilder/resultat/358x358/resultat-akne-fore-2.jpg',
+            image_after_small: '/bilder/resultat/358x358/resultat-akne-efter-2.jpg',
+            image_before_large: '/bilder/resultat/424x424/resultat-akne-fore-2.jpg',
+            image_after_large: '/bilder/resultat/424x424/resultat-akne-efter-2.jpg',
+            image_before_alt: 'Före',
+            image_before_title: 'Före',
+            image_after_alt: 'Efter',
+            image_after_title: 'Efter',
+            treatment: new ResultTreatment(
+                  duration: '<a href="acne.php" title="Akne">Akne</a>',
+            )
+      ),
+      new ResultCustomer(
+            image_before_small: '/bilder/resultat/358x358/resultat-rosacea-fore.jpg',
+            image_after_small: '/bilder/resultat/358x358/resultat-rosacea-efter.jpg',
+            image_before_large: '/bilder/resultat/424x424/resultat-rosacea-fore.jpg',
+            image_after_large: '/bilder/resultat/424x424/resultat-rosacea-efter.jpg',
+            image_before_alt: 'Före',
+            image_before_title: 'Före',
+            image_after_alt: 'Efter',
+            image_after_title: 'Efter',
+            treatment: new ResultTreatment(
+                  duration: '<a href="rosacea.php" title="Rosacea">Rosacea</a>',
+            )
+      ),
 );
 
 $show_all_results_label = "Se fler resultat";
