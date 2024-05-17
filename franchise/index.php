c8afa437fd63055ac3237fcd3344f69795507230
Patrick Minogue <patrick.minogue@gmail.com>
2024-05-17 10:10:19 +0000

    implement franchise.hero

--- franchise/index.php
+++ franchise/index.php
@@ -1,4 +1,16 @@
-<?php include_once($_SERVER['DOCUMENT_ROOT'] . '/config.php'); ?>
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/includes/models/link.php');
+$hero_title = "Start own / Franchise";
+$hero_content = "We at AcneSpecialisten offer different types of franchise collaborations, depending on what suits you best. You can rent a place in an already existing clinic or in different ways open your own, and eventually develop it. Apply below to become part of our concept - but with yourself as the boss!";
+$hero_image_small = 'https://via.placeholder.com/358x274.webp';
+$hero_image_large = 'https://via.placeholder.com/872x456.webp';
+$hero_image_alt = '';
+$hero_image_title = '';
+
+$application_link = new Link(label: "Send an Application", url: "#", title: "Click here to apply");
+
+?>
 <!DOCTYPE html>
 <html lang="<?php echo $lang ?>">
 
@@ -22,6 +34,24 @@
 <body>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'); ?>
     <main>
+        <div class="container" id="narrow-content">
+            <section id="hero">
+                <h1><?php echo $hero_title ?></h1>
+                <div id="hero-content"><?php echo $hero_content ?></div>
+                <div>
+                    <a href="<?php echo $application_link->url ?>" title="<?php echo $application_link->title ?>" class="button b200 expand"><?php echo $application_link->label ?></a>
+                </div>
+                <picture>
+                    <source media="(max-width: 449px)" srcset="<?php echo $hero_image_small ?>">
+                    <source media="(min-width: 450px)" srcset="<?php echo $hero_image_large ?>">
+                    <img src="<?php echo $hero_image_small ?>" alt="<?php echo $hero_image_alt ?>" title="<?php echo $hero_image_title ?>" />
+                </picture>
+            </section>
+            <section id="collaboration-types"></section>
+            <section id="concept"></section>
+            <section id="banner"></section>
+            <section id="perks"></section>
+        </div>
     </main>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
 </body>
