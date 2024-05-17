4189b158b5d9059bee04df3a842b35c7ac6cbae2
Patrick Minogue <patrick.minogue@gmail.com>
2024-05-17 09:44:12 +0000

    add skeleton page for franchise

---
+++ franchise/index.php
@@ -0,0 +1,29 @@
+<?php include_once($_SERVER['DOCUMENT_ROOT'] . '/config.php'); ?>
+<!DOCTYPE html>
+<html lang="<?php echo $lang ?>">
+
+<head>
+    <!-- TODO: Set title and meta tags -->
+    <title class="l10n">Acnespecialisten</title>
+    <meta name="description" content="" class="l10n">
+    <meta name="title" content="" class="l10n">
+    <meta name="keywords" content="" class="l10n">
+    <meta property="og:title" content="Acnespecialisten" />
+    <meta property="og:description" content="" class="l10n" />
+    <meta property="og:image" content="images/about-desktop.jpg" />
+    <meta property="twitter:title" content="Acnespecialisten" />
+    <meta property="twitter:description" content="" class="l10n" />
+    <meta property="twitter:image" content="images/about-desktop.jpg" />
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
+    <link rel="stylesheet" href="/styles/default-layout.css">
+    <link rel="stylesheet" href="/franchise/franchise.css">
+</head>
+
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'); ?>
+    <main>
+    </main>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
+</body>
+
+</html>
