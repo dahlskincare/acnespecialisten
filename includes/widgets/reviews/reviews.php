a089f328d64dc2a4ed212efa32ddaf0e107fdf18
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-06-03 15:16:52 +0200

    Update reviews.php

--- includes/widgets/reviews/reviews.php
+++ includes/widgets/reviews/reviews.php
@@ -69,7 +69,7 @@ $review_platforms =
             logo_url: 'images/brands/bokadirekt.svg',
             logo_alt: 'Bokadirekt',
             logo_title: 'Bokadirekt',
-            link: new Link('18 136 omdömen', 'https://www.bokadirekt.se/places/sveriges-skonhetscenter-acnespecialisten-sundbyberg-19300', 'Bokadirekt Omdömen'),
+            link: new Link('18 136 omdömen', 'https://www.bokadirekt.se/acnespecialisten/var', 'Bokadirekt Omdömen'),
             rating: 4.8,
             icons: $icon_registry['Bokadirekt'],
         )
