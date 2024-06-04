232925c919f0f6c10ee024100481b89e26f74131
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-06-04 10:57:20 +0200

    Update resultat.php

--- resultat.php
+++ resultat.php
@@ -82,9 +82,9 @@ $results_per_page = array(
     1 => array(
         new ResultCustomer(
             url: '',
-            image_before_small: '/bilder/resultat/178x238/resultat-akne-fore-1.jpg',
+            image_before_small: '',
+            image_before_large: '',
             image_after_small: '/bilder/resultat/178x238/resultat-akne-efter-1.jpg',
-            image_before_large: '/bilder/resultat/372x496/resultat-akne-fore-1.jpg',
             image_after_large: '/bilder/resultat/372x496/resultat-akne-efter-1.jpg',
             url_title: 'Akne resultat',
             image_before_alt: 'Före',
