793261bfdb2d681c205705cbf44dc1a27da55201
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-06-05 11:14:23 +0200

    Update microneedling.php

--- microneedling.php
+++ microneedling.php
@@ -56,7 +56,7 @@ $model = new Service(
     ),
 );
 
-$floating_box_title = "Hello";
+$floating_box_title = "Microneedling";
 $floating_box = 'Microneedling är en innovativ och effektiv behandling för att förbättra hudens kvalitet och adressera en mängd hudproblem.';
 
 $description_title = 'Vad är microneedling?';
