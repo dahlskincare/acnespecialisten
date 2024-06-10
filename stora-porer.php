8f5782efec0d4b279fc3bf2287c532e88afb2b2f
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-06-10 14:50:19 +0200

    t

--- stora-porer.php
+++ stora-porer.php
@@ -299,7 +299,7 @@ $products = array(
       new Service(
             title: 'Produkter för stora porer',
             duration: null,
-            price: 'Pris från 1495 kr',
+            price: 'Pris från 1395 kr',
             content: 'För att effektivt hantera och förebygga stora porer är daglig behandling med rätt produkter avgörande. Våra produkter för stora porer är speciellt utformade för att hålla din hud i balans och minska porernas synlighet.',
 
             image_small: 'images/services/200x200/produkter.webp',
