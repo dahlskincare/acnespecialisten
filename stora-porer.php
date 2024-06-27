35a7cb415c27dac436de0468e14aa18696b154f2
Engstrom-Peter <peter.engstrom@dahlskincare.com>
2024-06-27 12:07:07 +0200

    Update stora-porer.php

--- stora-porer.php
+++ stora-porer.php
@@ -342,24 +342,45 @@ $articles = array(
 );
 
 $results = array(
-      new LabelImage(
-        image_url: 'resultat/fore-och-efter-bilder-svar-acne-i-ansiktet.jpg',
-        image_alt: 'svår acne i ansiktet före och efter bilder',
-        image_title: '',
-        content: '',
-    ),
-    new LabelImage(
-        image_url: 'resultat/fore-och-efter-bilder-acne-i-ansiktet.jpg',
-        image_alt: 'acne i ansiktet kund före och efter bilder',
-        image_title: '',
-        content: '',
-    ),
-    new LabelImage(
-        image_url: 'resultat/fore-och-efter-bilder-acne-pa-huden.jpg',
-        image_alt: 'före och efter bilder acne på huden',
-        image_title: '',
-        content: '',
-    ),
+      new ResultCustomer(
+            image_before_small: '/bilder/resultat/358x358/resultat-akne-fore-1.jpg',
+            image_after_small: '/bilder/resultat/358x358/resultat-akne-efter-1.jpg',
+            image_before_large: '/bilder/resultat/424x424/resultat-akne-fore-1.jpg',
+            image_after_large: '/bilder/resultat/424x424/resultat-akne-efter-1.jpg',
+            image_before_alt: 'Före',
+            image_before_title: 'Före',
+            image_after_alt: 'Efter',
+            image_after_title: 'Efter',
+            treatment: new ResultTreatment(
+                  duration: '<a href="acne.php" title="Svår akne">Svår akne</a>',
+            )
+      ),
+      new ResultCustomer(
+            image_before_small: '/bilder/resultat/358x358/resultat-akne-fore-2.jpg',
+            image_after_small: '/bilder/resultat/358x358/resultat-akne-efter-2.jpg',
+            image_before_large: '/bilder/resultat/424x424/resultat-akne-fore-2.jpg',
+            image_after_large: '/bilder/resultat/424x424/resultat-akne-efter-2.jpg',
+            image_before_alt: 'Före',
+            image_before_title: 'Före',
+            image_after_alt: 'Efter',
+            image_after_title: 'Efter',
+            treatment: new ResultTreatment(
+                  duration: '<a href="acne.php" title="Akne">Akne</a>',
+            )
+      ),
+      new ResultCustomer(
+            image_before_small: '/bilder/resultat/358x358/resultat-rosacea-fore.jpg',
+            image_after_small: '/bilder/resultat/358x358/resultat-rosacea-efter.jpg',
+            image_before_large: '/bilder/resultat/424x424/resultat-rosacea-fore.jpg',
+            image_after_large: '/bilder/resultat/424x424/resultat-rosacea-efter.jpg',
+            image_before_alt: 'Före',
+            image_before_title: 'Före',
+            image_after_alt: 'Efter',
+            image_after_title: 'Efter',
+            treatment: new ResultTreatment(
+                  duration: '<a href="rosacea.php" title="Rosacea">Rosacea</a>',
+            )
+      ),
 );
 
 $show_all_results_label = "Se fler resultat";
