0708668285316b69206432466281c4938df5976b
Mac <68950292+macentoshMD@users.noreply.github.com>
2024-07-23 08:32:37 +0200

    Update alma-hybrid-co2-laser.php

--- alma-hybrid-co2-laser.php
+++ alma-hybrid-co2-laser.php
@@ -430,7 +430,7 @@ $treatment_areas = array(
         image_alt: 'Kombinera områden',
         image_title: 'Kombinera områden',
         title: 'Kombinera områden',
-        description: '<p class="p200">Kombinera flera områden för ett jämnare resultat.</p>',
+        description: '<p class="p200">Kombinera flera områden för ett utökat och jämnare resultat.</p>',
         items: array(
             new TreatmentAreaItem(
                 image: '/icons/1x.svg',
