a8860fba0212dd51e4a74b637c76406350af9306
Patrick Minogue <patrick.minogue@gmail.com>
2024-07-23 14:30:07 +0000

    implement rich_reviews

--- includes/rich_content/review.php
+++ includes/rich_content/review.php
@@ -1,22 +1,25 @@
-<?php if (isset($rich_review)) { ?>
+<?php if (isset($rich_reviews)) { ?>
     <script type="application/ld+json">
-        {
-            "@context": "https://schema.org",
-            "@type": "Review",
-            "author": {
-                "@type": "Person",
-                "name": "<?php echo $rich_review->name ?>"
-            },
-            "reviewBody": "<?php echo $rich_review->body ?>",
-            "reviewRating": {
-                "@type": "Rating",
-                "ratingValue": "<?php echo $rich_review->rating ?>",
-                "bestRating": "5"
-            },
-            "itemReviewed": {
-                "@type": "Service",
-                "name": "<?php echo $rich_review->service_name ?>"
-            }
-        }
+        [
+            <?php foreach ($rich_reviews as $rich_review) { ?> {
+                    "@context": "https://schema.org",
+                    "@type": "Review",
+                    "author": {
+                        "@type": "Person",
+                        "name": "<?php echo $rich_review->name ?>"
+                    },
+                    "reviewBody": "<?php echo $rich_review->body ?>",
+                    "reviewRating": {
+                        "@type": "Rating",
+                        "ratingValue": "<?php echo $rich_review->rating ?>",
+                        "bestRating": "5"
+                    },
+                    "itemReviewed": {
+                        "@type": "Service",
+                        "name": "<?php echo $rich_review->service_name ?>"
+                    }
+                }
+            <?php } ?>
+        ]
     </script>
 <?php } ?>
