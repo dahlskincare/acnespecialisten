993b6fc13917473ae69437d32dc053da307fa440
Mac <68950292+macentoshMD@users.noreply.github.com>
2024-07-24 10:21:43 +0200

    Update acnebehandling.php

--- acnebehandling.php
+++ acnebehandling.php
@@ -16,7 +16,7 @@ $model = new Service(
     title: 'Aknebehandlingar',
     duration: null,
     price: 'Från 1595 kr',
-    content: '<p class="p200">Här har vi samlat de bästa aknebehandlingarna som hjälper dig att effektivt få bort akne oavsett typ. Är du osäker på vilken aknebehandling som bäst passar din hudtyp? Oavsett om det är hormonell acne behandling eller annan typ av akne rekomenderar vi att börja med en kostnadsfri hudkonsultation hos oss på AcneSpecialisten så hjälper vi dig att få bort akne.</p>',
+    content: '<p class="p200">Här har vi samlat de bästa aknebehandlingarna för att effektivt ta bort akne, oavsett typ. Är du osäker på vilken behandling som bäst passar din hudtyp? Oavsett om det gäller hormonell akne eller annan typ av akne, rekommenderar vi att börja med en kostnadsfri hudkonsultation hos oss på AcneSpecialisten. Vi hjälper dig att skräddarsy en behandlingsplan för att effektivt bekämpa och ta bort din akne.</p>',
     image_small: 'images/treatments/small/acne-scars.jpg',
     image_large: 'images/treatments/large/acne-scars.jpg',
     image_alt: 'Bild som illustrerar akne',
