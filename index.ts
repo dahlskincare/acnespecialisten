4acf55ab81bbe574783b7cc93a102b5545c02161
Patrick Minogue <patrick.minogue@gmail.com>
2024-08-01 11:12:10 +0000

    use webm videos

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 
 let video = document.querySelector('#banner-video') as HTMLVideoElement;
 if (document.body.clientWidth > 800) {
-    video.src = 'videos/banner.mp4';
+    video.src = 'videos/banner.webm';
 } else {
-    video.src = 'videos/banner-square.mp4';
-}
+    video.src = 'videos/banner-square.webm';
+} 
