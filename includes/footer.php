1dc3e87fbbe4d8146708cccdfd0b1b2ee78d3cd5
Patrick Minogue <patrick.minogue@gmail.com>
2025-01-07 14:00:38 +0000

    move stuff down to footer

--- includes/footer.php
+++ includes/footer.php
@@ -457,6 +457,83 @@
         </div>
 </footer>
 
+<!-- Google Tag Manager -->
+<script data-cookieconsent="ignore">
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-PFRBPG9K');
+</script>
+<!-- End Google Tag Manager -->
+
+<script>
+    document.onreadystatechange = function() {
+        if (document.readyState === "complete") {
+            if (sessionStorage.getItem('intercomLoaded') && typeof window.Intercom === "function") {
+                window.Intercom('reattach_activator');
+                window.Intercom('update', window.intercomSettings);
+            } else {
+                window.intercomSettings = {
+                    app_id: "p785iyzu"
+                };
+                var i = function() {
+                    i.c(arguments);
+                };
+                i.q = [];
+                i.c = function(args) {
+                    i.q.push(args);
+                };
+                window.Intercom = i;
+
+                var s = document.createElement("script");
+                s.type = "text/javascript";
+                s.async = true;
+                s.src = "https://widget.intercom.io/widget/p785iyzu";
+                var x = document.getElementsByTagName("script")[0];
+                x.parentNode.insertBefore(s, x);
+
+                s.onload = function() {
+                    if (typeof window.Intercom === "function") {
+                        window.Intercom('boot', window.intercomSettings);
+                    } else {
+                        console.error("Intercom laddades, men är inte tillgängligt.");
+                    }
+                };
+
+                s.onerror = function() {
+                    console.error("Intercom-skriptet kunde inte laddas.");
+                };
+
+                sessionStorage.setItem('intercomLoaded', true);
+            }
+
+
+            // Copy gclid url param to all booking links
+            var urlParams = new URLSearchParams(window.location.search);
+            var gclid = urlParams.get('gclid');
+            if (gclid) {
+                var links = document.querySelectorAll('a');
+                for (var i = 0; i < links.length; i++) {
+                    var link = links[i];
+                    if (link.href.startsWith('https://boka.acnespecialisten.se')) {
+                        link.href = link.href + '&gclid=' + gclid;
+                    }
+                }
+            }
+        }
+    }
+</script>
+
 <script>
     window.addEventListener('scroll', onScroll, {
         passive: true
