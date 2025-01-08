51a84b9a122a80f82fdae5a5ee257830f49d5ad2
Patrick Minogue <patrick.minogue@gmail.com>
2025-01-08 13:02:39 +0000

    move tag manager to head

--- includes/footer.php
+++ includes/footer.php
@@ -462,22 +462,6 @@
 
 <script>
     window.addEventListener('load', function() {
-        // Tag manager
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-PFRBPG9K');
-
         // add a 5 second delay
         setTimeout(function() {
             if (sessionStorage.getItem('intercomLoaded') && typeof window.Intercom === "function") {
