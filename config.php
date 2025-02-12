b84e729a5342c67f81437e2ce12402dbb7fe6ecb
Patrick Minogue <patrick.minogue@gmail.com>
2025-02-12 09:46:44 +0000

    add strict-transport-security-header

--- config.php
+++ config.php
@@ -1,4 +1,6 @@
 <?php
+// Add the Strict-Transport-Security header
+header('Strict-Transport-Security: max-age=31536000; includeSubDomains');
 $lang = 'sv';
 
 require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
