b540be909a6958a1cbadfb8b1e31cb20a272578a
Mac <68950292+macentoshMD@users.noreply.github.com>
2025-02-17 16:06:42 +0100

    uppdatera schema org och local

--- includes/rich_content/organization.php
+++ includes/rich_content/organization.php
@@ -5,19 +5,31 @@
         "name": "AcneSpecialisten",
         "url": "https://www.acnespecialisten.se",
         "logo": "https://acnespecialisten.se/images/logo-green.svg",
+        "description": "En hudvårdskedja specialiserad på behandlingar och hudvård för människor med hudproblem som akne, finnar, rosacea. Vi har samlat över 25000 omdömen från Google, Trustpilot och Bokadirekt med ett genomsnitt på 4,8 av 5.",
+
         "contactPoint": {
             "@type": "ContactPoint",
-            "telephone": "+468296101",
+            "telephone": "+46-8-296101",
             "contactType": "Customer Service"
         },
         "sameAs": [
             "https://www.facebook.com/acnespecialisten",
-            "https://www.instagram.com/acnespecialisten"
+            "https://www.instagram.com/acnespecialisten/",
+            "https://se.trustpilot.com/review/acnespecialisten.se",
+            "https://maps.app.goo.gl/FXp6WYFytSQYFWXb6",
+            "https://maps.app.goo.gl/nCehKyfxoxP2N3po6",
+            "https://maps.app.goo.gl/8RGWbXiDbQTa9LUb9",
+            "https://www.bokadirekt.se/places/sveriges-skonhetscenter-acnespecialisten-sundbyberg-19300",
+            "https://www.bokadirekt.se/places/sveriges-skonhetscenter-acnespecialisten-sodermalm-19301",
+            "https://www.bokadirekt.se/places/sveriges-skonhetscenter-acnespecialisten-ostermalm-43559"
         ],
+
         "aggregateRating": {
-    "@type": "AggregateRating",
-    "ratingValue": "4.9",
-    "reviewCount": "718"
-  }
+            "@type": "AggregateRating",
+            "ratingValue": "4.7",
+            "reviewCount": "25478",
+            "bestRating": "5",
+            "worstRating": "1"
+        }
     }
 </script>
