75d7e6cbac2e8bbc69a9f39a09a8e1fbab64daeb
Mac <68950292+macentoshMD@users.noreply.github.com>
2025-02-18 15:24:01 +0100

    Uppdatera rich products

--- includes/rich_content/product.php
+++ includes/rich_content/product.php
@@ -2,18 +2,19 @@
     <script type="application/ld+json">
         {
             "@context": "https://schema.org/",
-            "@type": "Product",
+            "@type": "Service",
             "name": "<?php echo $rich_product->name ?>",
+            "serviceType": "Ansiktsbehandlingar mot akne och andra hudproblem",
             "image": "<?php echo $rich_product->image ?>",
             "description": "<?php echo $rich_product->description ?>",
-            "brand": {
-                "@type": "Brand",
+            "provider": {
+                "@type": "HealthAndBeautyBusiness",
                 "name": "<?php echo $rich_product->brand ?>"
             },
             "aggregateRating": {
                 "@type": "AggregateRating",
                 "ratingValue": "<?php echo $rich_product->ratingValue ?>",
-                "reviewCount": "<?php echo $rich_product->ratingCount ?>"
+                "reviewCount": "<?php echo $rich_product->ratingCount ?>",
                 "bestRating": "5",
                 "worstRating": "1"
             },
@@ -25,4 +26,27 @@
         }
     </script>
 
-<?php } ?>
+<?php } else { ?>
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org/",
+            "@type": "Service",
+            "name": "Hudvårdsbehandlingar mot hudproblem",
+            "serviceType": "Ansiktsbehandlingar",
+            "image": "https://acnespecialisten.se/bilder/hudbehandlingar/424x456/ansiktsbehandlingar-mot-problemhy.webp",
+            "description": "Våra behandlingar ger dig en renare och friskare hud. Redan efter första gången kommer du att märka skillnad och vi anpassar alltid ansiktsbehandlingen efter just din hudtyp.",
+            "Provider": {
+                "@type": "HealthAndBeautyBusiness",
+                "name": "AcneSpecialisten"
+            },
+            "aggregateRating": {
+                "@type": "AggregateRating",
+                "ratingValue": "4.9",
+                "reviewCount": "718",
+                "bestRating": "5",
+                "worstRating": "1"
+            }
+        }
+    </script>
+
+    <?php } ?>
